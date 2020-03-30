cf92ef380841fb83436678cae595479f5fb68711
cicimen <egemengulkilik@gmail.com>
2020-03-30 05:57:31 +0300

    Visilabs swift eklendi.

---
+++ VisilabsIOS/Classes/Visilabs.swift
@@ -0,0 +1,14 @@
+
+open class Visilabs{
+    
+    private var logUrl :String?
+    
+    init(logUrl: String?) {
+        self.logUrl = logUrl
+    }
+    
+    open class func initialize(){
+        
+    }
+    
+}
