bdc095d882965bde086acbbc95b9a11bde6b7c20
cicimen <egemengulkilik@gmail.com>
2020-04-01 02:44:39 +0300

    private init method complete

--- VisilabsIOS/Classes/Visilabs.swift
+++ VisilabsIOS/Classes/Visilabs.swift
@@ -1,14 +1,38 @@
 
 open class Visilabs{
     
-    private var logUrl :String?
+    static var visilabs: Visilabs?
     
-    init(logUrl: String?) {
-        self.logUrl = logUrl
-    }
+    private var organizationID : String
+    private var siteID : String
+    private var loggerURL : String
+    private var dataSource : String
+    private var realTimeURL : String
+    private var channel : String
+    private var requestTimeoutInSeconds : Int
+    private var restURL : String
+    private var encryptedDataSource : String
+    private var targetURL : String
+    private var actionURL : String
+    private var geofenceURL : String
+    private var geofenceEnabled : Bool
+    private var maxGeofenceCount : Int
     
-    open class func initialize(){
-        
+    private init(organizationID: String, siteID: String, loggerURL: String, dataSource: String, realTimeURL: String, channel: String, requestTimeoutInSeconds: Int, restURL: String, encryptedDataSource: String, targetURL: String, actionURL: String, geofenceURL: String, geofenceEnabled: Bool, maxGeofenceCount: Int) {
+        self.organizationID = organizationID
+        self.siteID = siteID
+        self.loggerURL = loggerURL
+        self.dataSource = dataSource
+        self.realTimeURL = realTimeURL
+        self.channel = channel
+        self.requestTimeoutInSeconds = requestTimeoutInSeconds
+        self.restURL = restURL
+        self.encryptedDataSource = encryptedDataSource
+        self.targetURL = targetURL
+        self.actionURL = actionURL
+        self.geofenceURL = geofenceURL
+        self.geofenceEnabled = geofenceEnabled
+        self.maxGeofenceCount = maxGeofenceCount
     }
     
 }
