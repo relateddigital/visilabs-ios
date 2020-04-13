336ab9bd8ecb26426a66b719416db06b5e92181a
cicimen <egemengulkilik@gmail.com>
2020-04-14 01:26:49 +0300

    VisilabsResponseDelegate implement

--- VisilabsResponseDelegate.swift
+++ VisilabsResponseDelegate.swift
@@ -5,4 +5,19 @@
 //  Created by Egemen on 14.04.2020.
 //
 
-import Foundation
+/**
+The delegate class to process the response data. If callback blocks are not
+used in the API requests, this protocol should be implemented and assigned to
+an instance of VisilabsAction
+*/
+
+//TODO: NSObjectProtocol'ü inherit etmesine gerek var mı?
+//protocol VisilabsResponseDelegate: NSObjectProtocol {
+protocol VisilabsResponseDelegate {
+    /// Executed if the request is successful
+    /// - Parameter res: The response object
+    func requestDidSucceed(with res: VisilabsResponse?)
+    /// Executed if the request is failed
+    /// - Parameter res: The response object
+    func requestDidFail(with res: VisilabsResponse?)
+}
