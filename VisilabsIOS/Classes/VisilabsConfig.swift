c32926a36243b21502d4514c16ef5b18a8d875b7
cicimen <egemengulkilik@gmail.com>
2020-04-16 02:36:01 +0300

    VisilabsConfig constants complete

--- VisilabsIOS/Classes/VisilabsConfig.swift
+++ VisilabsIOS/Classes/VisilabsConfig.swift
@@ -7,6 +7,35 @@
 
 import Foundation
 
-class VisilabsConfig {
-    var visilabsParameters : [VisilabsParameter]?
+struct VisilabsConfig {
+    
+    static let ORGANIZATIONID_KEY = "OM.oid"
+    static let SITEID_KEY = "OM.siteID"
+    static let COOKIEID_KEY = "OM.cookieID"
+    static let EXVISITORID_KEY = "OM.exVisitorID"
+    static let ZONE_ID_KEY = "OM.zid"
+    static let BODY_KEY = "OM.body"
+
+    static let LATITUDE_KEY = "OM.latitude"
+    static let LONGITUDE_KEY = "OM.longitude"
+
+    static let ACT_ID_KEY = "actid"
+    static let ACT_KEY = "act"
+
+    static let TOKENID_KEY = "OM.sys.TokenID"
+    static let APPID_KEY = "OM.sys.AppID"
+
+
+    static let LOGGER_URL = "lgr.visilabs.net"
+    static let REAL_TIME_URL = "rt.visilabs.net"
+    static let LOAD_BALANCE_PREFIX = "NSC"
+    static let OM_3_KEY = "OM.3rd"
+
+
+    static let FILTER_KEY = "OM.w.f"
+    static let APIVER_KEY = "OM.apiver"
+
+    static let GEO_ID_KEY = "OM.locationid"
+
+    static let TRIGGER_EVENT_KEY = "OM.triggerevent"
 }
