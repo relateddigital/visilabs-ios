3f7c5bbbf5cef9746120ebfc59d3cc9ec3a1794c
cicimen <egemengulkilik@gmail.com>
2020-04-16 02:22:00 +0300

    VisilabsDataManager implement

--- VisilabsIOS/Classes/VisilabsDataManager.swift
+++ VisilabsIOS/Classes/VisilabsDataManager.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+
+class VisilabsDataManager {
+    
+    class func save(_ key: String?, withObject value: Any?) {
+        let defaults = UserDefaults.standard
+        defaults.set(value, forKey: key ?? "")
+        defaults.synchronize()
+    }
+
+    class func read(_ key: String?) -> Any? {
+        let defaults = UserDefaults.standard
+        return defaults.object(forKey: key ?? "")
+    }
+
+    class func remove(_ key: String?) {
+        let defaults = UserDefaults.standard
+        defaults.removeObject(forKey: key ?? "")
+        defaults.synchronize()
+    }
+    
+}
