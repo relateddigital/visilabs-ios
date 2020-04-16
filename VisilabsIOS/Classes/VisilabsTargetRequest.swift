77d35a72f2516a323c0a67ff688f89bc339531df
cicimen <egemengulkilik@gmail.com>
2020-04-16 18:12:38 +0300

    VisilabsTargetRequest buildURL and cleanParameters methods implement

--- VisilabsIOS/Classes/VisilabsTargetRequest.swift
+++ VisilabsIOS/Classes/VisilabsTargetRequest.swift
@@ -10,6 +10,27 @@ import Foundation
 class VisilabsTargetRequest: VisilabsAction {
     var zoneID: String?
     var productCode: String?
-    var properties: [AnyHashable : Any]?
+    var properties: [String : Any]?
     var filters: [VisilabsTargetFilter]?
+    
+    override func buildURL() -> URL? {
+        var targetURL = Visilabs.callAPI()!.targetURL
+        let queryParameters = getParametersAsQueryString()
+        targetURL = targetURL + (queryParameters)
+        let uri = URL(string: targetURL!)
+        return uri
+    }
+    
+    func cleanParameters() {
+        if properties != nil {
+            for propKey in properties!.keys {
+                if !propKey.isEqual(VisilabsConfig.ORGANIZATIONID_KEY) && !propKey.isEqual(VisilabsConfig.SITEID_KEY) && !propKey.isEqual(VisilabsConfig.EXVISITORID_KEY) && !propKey.isEqual(VisilabsConfig.COOKIEID_KEY) && !propKey.isEqual(VisilabsConfig.ZONE_ID_KEY) && !propKey.isEqual(VisilabsConfig.BODY_KEY) && !propKey.isEqual(VisilabsConfig.TOKENID_KEY) && !propKey.isEqual(VisilabsConfig.APPID_KEY) && !propKey.isEqual(VisilabsConfig.APIVER_KEY) && !propKey.isEqual(VisilabsConfig.FILTER_KEY) {
+                    continue
+                } else {
+                    properties?.removeValue(forKey: propKey)
+                }
+            }
+        }
+    }
+    
 }
