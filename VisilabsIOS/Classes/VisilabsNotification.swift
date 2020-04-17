3eee5e74ad59afbade366023c4f5a2ef32dec2aa
cicimen <egemengulkilik@gmail.com>
2020-04-17 14:40:33 +0300

    VisilabsNotification properties set

--- VisilabsIOS/Classes/VisilabsNotification.swift
+++ VisilabsIOS/Classes/VisilabsNotification.swift
@@ -7,6 +7,27 @@
 
 import Foundation
 
-class VisilabsNotification{
-    
+let VisilabsNotificationTypeMini: String? = nil
+let VisilabsNotificationTypeFull: String? = nil
+
+class VisilabsNotification {
+    private(set) var anId = 0
+    var type: String?
+    var title: String?
+    var body: String?
+    var imageURL: URL?
+    var buttonText: String?
+    var buttonURL: URL?
+    var visitorData: String?
+    var visitData: String?
+    var queryString: String?
+    var image: Data?
+
+    /*
+    convenience init?(jsonObject object: [AnyHashable : Any]?) {
+    }
+
+    init() {
+    }
+    */
 }
