ce5ec29886ab7e72e98742ec292c4c3604c8d262
cicimen <egemengulkilik@gmail.com>
2020-04-23 21:38:23 +0300

    VisilabsGeofenceStatus class create and VisilabsGeofenceBridge methods add

--- VisilabsIOS/Classes/VisilabsGeofenceBridge.swift
+++ VisilabsIOS/Classes/VisilabsGeofenceBridge.swift
@@ -7,17 +7,53 @@
 
 import Foundation
 
+/**
+Bridge for handle geofence module notifications.
+*/
 class VisilabsGeofenceBridge {
+    
+    /**
+    Static entry point for bridge init.
+    */
     class func bridgeHandler(_ notification: Notification?) {
-        /*
-        VisilabsGeofenceApp.sharedInstance().isDefaultLocationServiceEnabled = true
+        
+        VisilabsGeofenceApp.sharedInstance()?.isDefaultLocationServiceEnabled = true
 
+        //TODO: SH_LMBridge_CreateLocationManager'ları visilabsconfig e taşı
         NotificationCenter.default.addObserver(self, selector: #selector(createLocationManagerHandler(_:)), name: NSNotification.Name("SH_LMBridge_CreateLocationManager"), object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(setGeofenceTimestampHandler(_:)), name: NSNotification.Name("SH_LMBridge_SetGeofenceTimestamp"), object: nil)
 
-        NotificationCenter.default.post(name: NSNotification.Name("SH_LMBridge_SetGeofenceTimestamp"), object: nil, userInfo: [
-        "timestamp": NONULL("2016-01-01")
-        ])
- */
+        //TODO: timestamp tarihini değiştirirsem ne olur?, SH_LMBridge_SetGeofenceTimestamp i visilabsconfig e taşı
+        NotificationCenter.default.post(name: NSNotification.Name("SH_LMBridge_SetGeofenceTimestamp"), object: nil, userInfo: [ "timestamp": "2016-01-01"])
+ 
+    }
+    
+    // MARK: - private functions
+    
+    class func geofenceTimestampTimer() -> Timer? {
+        return nil
+    }
+
+    class func setGeofenceTimestampTimer(_ timer: Timer?) {
+        
+    }
+    
+    //TODO: @objc kaldırılabiliyor mu kontrol et, private yapılabiliyor mu kontrol et
+    @objc class func createLocationManagerHandler(_ notification: Notification?) {
+        if VisilabsGeofenceApp.sharedInstance()!.locationManager == nil {
+            VisilabsGeofenceApp.sharedInstance()!.locationManager = VisilabsGeofenceLocationManager.sharedInstance() //cannot move to `init` because it starts `startMonitorGeoLocationStandard` when create.
+        }
+    }
+
+    //TODO: @objc kaldırılabiliyor mu kontrol et
+    @objc class func setGeofenceTimestampHandler(_ notification: Notification?) {
+        if self.geofenceTimestampTimer() != nil {
+            self.geofenceTimestampTimer()!.invalidate()
+        }
+        self.setGeofenceTimestampTimer(nil)
+        
+        //TODO: buradaki timeInterval i visilabsconfig e taşı
+        self.setGeofenceTimestampTimer(Timer.scheduledTimer(timeInterval: 900, target: self, selector: #selector(setHandler), userInfo: nil, repeats: true))
+        self.geofenceTimestampTimer()!.fire()
     }
 }
--- VisilabsIOS/Classes/VisilabsGeofenceStatus.swift
+++ VisilabsIOS/Classes/VisilabsGeofenceStatus.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+/// The object to handle geofence status inside SHAppStatus.
+class VisilabsGeofenceStatus: NSObject {
+    /// Singleton for get app status instance.
+    class func sharedInstance() -> VisilabsGeofenceStatus? {
+        return nil
+    }
+
+    /// Match to `app_status` dictionary's `geofences`. It's a time stamp of server provided geofence list. If the time stamp is newer than client fetch time, client should fetch geofence list again and monitor new list; if the time stamp is NULL or empty, client should clear cached geofence and stop monitor.
+    var geofenceTimestamp: String?
+    var arrayGeofenceFetchList: [AnyHashable]?
+}
