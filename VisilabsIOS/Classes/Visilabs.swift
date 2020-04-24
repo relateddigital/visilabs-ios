5f9627d630fe4df713fc68f700d26cf04f91e10e
umutalparslan <umut_alparslan@hotmail.com>
2020-04-24 11:50:15 +0300

    Added Custom Event & Urlize Props

--- VisilabsIOS/Classes/Visilabs.swift
+++ VisilabsIOS/Classes/Visilabs.swift
@@ -396,6 +396,7 @@ open class Visilabs{
                 setCookieID()
             }
 
+            
             exVisitorID = exvisitorid
             if !NSKeyedArchiver.archiveRootObject(exVisitorID!, toFile: exVisitorIDFilePath()!) {
                 print("Visilabs: WARNING - Unable to archive new identity!!!")
