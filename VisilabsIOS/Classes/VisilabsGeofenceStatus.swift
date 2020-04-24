d55d077a44ccdf682881640a212588d368043dc8
cicimen <egemengulkilik@gmail.com>
2020-04-25 02:03:36 +0300

    VisilabsGeofenceStatus implementation complete

--- VisilabsIOS/Classes/VisilabsGeofenceStatus.swift
+++ VisilabsIOS/Classes/VisilabsGeofenceStatus.swift
@@ -178,6 +178,101 @@ class VisilabsGeofenceStatus: NSObject {
         }
     }
     
+    //get VisilabsServerGeofence list, subset of self.arrayGeofenceFetchList, which match this region. It searches both parent and child list.
+    func findServerGeofence(for region: CLRegion?) -> VisilabsServerGeofence? {
+        if !(region is CLCircularRegion) {
+            return nil
+        }
+        let geoRegion = region as? CLCircularRegion
+        for geofence in arrayGeofenceFetchList {
+            let matchGeofence = searchSelfAndChild(geofence, for: geoRegion)
+            if matchGeofence != nil {
+                return matchGeofence
+            }
+        }
+        return nil
+    }
+    
+    //search recursively to match
+    func searchSelfAndChild(_ geofence: VisilabsServerGeofence?, for geoRegion: CLCircularRegion?) -> VisilabsServerGeofence? {
+        if geofence?.isEqual(toCircleRegion: geoRegion) != nil {
+            return geofence
+        }
+        if let arrayNodes = geofence?.arrayNodes {
+            for childGeoFence in arrayNodes {
+                let match = searchSelfAndChild(childGeoFence, for: geoRegion)
+                if match != nil {
+                    return match
+                }
+            }
+        }
+        return nil
+    }
+    
+    //When a geofence outside, mark itself and child (if has) to be outside, send out geofene logline for previous inside leave geofence too. Make it a separate function because it's recurisive.
+    func markSelfAndChildGeofenceOutside( _ geofence: VisilabsServerGeofence?) {
+        if geofence?.isInside != nil {
+            geofence?.isInside = false
+            if geofence?.isLeaves != nil { //for leave go outside, send logline.
+                //sendLog(forGeoFence: geofence, isInside: false) //kullanılmıyor.
+            } else { //if parent geofence out, make all child geofence outside too.
+                if let arrayNodes = geofence?.arrayNodes {
+                    for childGeofence in arrayNodes {
+                        markSelfAndChildGeofenceOutside(childGeofence) //recurisively do it as child geofence may contains child too.
+                    }
+                }
+            }
+        }
+    }
+    
+    //monitor when a region state change.
+    func regionStateChangeNotificationHandler(_ notification: Notification?) {
+        //use state change instead of didEnterRegion/didExitRegion because when startMonitorRegion, state change delegate is called, didEnter/ExitRegion delegate not called until next enter/exit.
+        //TODO: Region ve RegionState kısmını VisilabsConfig e aktar
+        if let region = notification?.userInfo?["Region"] as? CLRegion {
+            //TODO: NSNumber'lardan kurtul
+            let regionState = CLRegionState(rawValue: (notification?.userInfo?["RegionState"] as? NSNumber)?.intValue ?? 0)
+            if regionState == .inside {
+                if (region is CLCircularRegion) {
+                    if let geofence = findServerGeofence(for: region), !geofence.isInside /*only take action if change*/ {
+                        geofence.isInside = true
+                        //TODO: "APPSTATUS_GEOFENCE_FETCH_LIST" i VisilabsConfig e taşı
+                        UserDefaults.standard.set(VisilabsServerGeofence.serialize(toArrayDict: arrayGeofenceFetchList), forKey: "APPSTATUS_GEOFENCE_FETCH_LIST")
+                        UserDefaults.standard.synchronize()
+                        if geofence.isLeaves { //if this is actual geofence, send enter logline and it's done
+                            //sendLog(forGeoFence: geofence, isInside: true) //kullanılmıyor.
+                        } else {
+                            stopMonitorPreviousGeofencesOnly() //This is a tricky: parent fence may overlap. Case 1: simple case, if parent fence not overlap, enter this one means all others are outside, so stop all other parent fences and add this one's child. Case 2: if parent fence P1 overlap with parent fence P2, P1 is already inside, now enter P2. This check will keep P1 and P1's child fence in monitoring, while later add P2 and its child.
+                            startMonitorGeofences(geofence.arrayNodes) //geofence itself is already monitor
+                        }
+                    }
+                }
+                else if regionState == .outside {
+                    if (region is CLCircularRegion) {
+                        if let geofence = findServerGeofence(for: region), geofence.isInside /*only take action if change*/ {
+                            self.markSelfAndChildGeofenceOutside(geofence) //recursively mark this geofence and its child all outside. It also sends exit logline for child if necessary, because if parent exit before child leave, child will be marked as outside, and this logic will not enter when child leave detect outside.
+                            //TODO: "APPSTATUS_GEOFENCE_FETCH_LIST" i VisilabsConfig e taşı
+                            UserDefaults.standard.set(VisilabsServerGeofence.serialize(toArrayDict: arrayGeofenceFetchList), forKey: "APPSTATUS_GEOFENCE_FETCH_LIST")
+                            UserDefaults.standard.synchronize()
+                            if !geofence.isLeaves{ //if this is inner geofence, stop monitor its child geofence, add itself and it's same level.
+                                self.stopMonitorPreviousGeofencesOnly()//in case overlap and in another parent geofence, this will keep it un-affected.
+                                if let parentGeofence = geofence.parentFence, parentGeofence.isInside {
+                                    //Move out from a inner geofence, if its parent geofence is still inside, monitor itself and its same level. Cannot monitor top level in this case, because if monitor top level, its parent fence is already monitored, and not stop/add again, no enter happens, so the same level geofence (some maybe leave geofence), will not be monitored.
+                                    self.startMonitorGeofences(parentGeofence.arrayNodes)
+                                }
+                                else{
+                                    self.startMonitorGeofences(self.arrayGeofenceFetchList) //monitor all top level geofences.
+                                }
+                            }
+                        }
+                        
+                    }
+                }
+            }
+        }
+        
+    }
+    
     
     //TODO: DispatchSemaphore u kontrol et
     private static let visilabsParseDateFormatter_semaphore = { () -> DispatchSemaphore in
