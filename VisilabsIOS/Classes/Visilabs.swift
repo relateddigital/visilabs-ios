1d978d4e96f6a0b89a19d8e76005656e6476ba2e
cicimen <egemengulkilik@gmail.com>
2020-04-29 05:27:14 +0300

    Visilabs send lock add to response

--- VisilabsIOS/Classes/Visilabs.swift
+++ VisilabsIOS/Classes/Visilabs.swift
@@ -713,6 +713,9 @@ open class Visilabs : NSObject, VisilabsNotificationViewControllerDelegate {
                         }else{
                             if let res = response as? HTTPURLResponse {
                                 if res.statusCode == 200 || res.statusCode == 304{
+                                    Visilabs.visilabsLockingQueue.sync {
+                                    
+                                    }
                                     
                                 }
                             }
