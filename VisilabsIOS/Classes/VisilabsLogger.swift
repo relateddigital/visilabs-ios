04c75550546617c88778197de5a921f922843abe
cicimen <egemengulkilik@gmail.com>
2020-05-03 00:48:31 +0300

    VisilabsLogMessage struct create

--- VisilabsIOS/Classes/VisilabsLogger.swift
+++ VisilabsIOS/Classes/VisilabsLogger.swift
@@ -21,6 +21,31 @@ enum VisilabsLogLevel: String {
     case error
 }
 
+struct VisilabsLogMessage {
+    /// The file where this log message was created
+    let file: String
+
+    /// The function where this log message was created
+    let function: String
+
+    /// The text of the log message
+    let text: String
+
+    /// The level of the log message
+    let level: VisilabsLogLevel
+
+    init(path: String, function: String, text: String, level: VisilabsLogLevel) {
+        if let file = path.components(separatedBy: "/").last {
+            self.file = file
+        } else {
+            self.file = path
+        }
+        self.function = function
+        self.text = text
+        self.level = level
+    }
+}
+
 class VisilabsLogger{
     private static var enabledLevels = Set<VisilabsLogLevel>()
     
