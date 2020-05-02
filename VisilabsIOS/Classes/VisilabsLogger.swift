f2fc23316a7220f11b7c872ac85f674fa27017bb
cicimen <egemengulkilik@gmail.com>
2020-05-03 00:38:51 +0300

    VisilabsLogLevel enum define

--- VisilabsIOS/Classes/VisilabsLogger.swift
+++ VisilabsIOS/Classes/VisilabsLogger.swift
@@ -7,6 +7,20 @@
 
 import Foundation
 
+enum VisilabsLogLevel: String {
+    /// Logging displays *all* logs and additional debug information that may be useful to a developer
+    case debug
+
+    /// Logging displays *all* logs (**except** debug)
+    case info
+
+    /// Logging displays *only* warnings and above
+    case warning
+
+    /// Logging displays *only* errors and above
+    case error
+}
+
 class VisilabsLogger{
     
     
