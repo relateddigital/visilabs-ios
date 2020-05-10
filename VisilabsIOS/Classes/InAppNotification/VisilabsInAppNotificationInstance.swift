8a744935385915f6d10b1525967bd9e170da21ce
cicimen <egemengulkilik@gmail.com>
2020-05-11 00:24:23 +0300

    VisilabsInAppNotificationInstance file VisilabsInAppNotificationsDelegate and VisilabsInAppNotificationType add

--- VisilabsIOS/Classes/InAppNotification/VisilabsInAppNotificationInstance.swift
+++ VisilabsIOS/Classes/InAppNotification/VisilabsInAppNotificationInstance.swift
@@ -7,7 +7,21 @@
 
 import Foundation
 
+protocol VisilabsInAppNotificationsDelegate {
+    func notificationDidShow(_ notification: VisilabsInAppNotification)
+    func trackNotification(_ notification: VisilabsInAppNotification, event: String, properties: [String:String]?)
+}
 
+enum VisilabsInAppNotificationType : String {
+    case mini = "mini"
+    case full = "full"
+    case image_text_button = "image_text_button"
+    case full_image = "full_image"
+    case nps = "nps"
+    case image_button = "image_button"
+    case smile_rating = "smile_rating"
+    case unknown = "unknown"
+}
 
 
 class VisilabsInAppNotificationInstance {
