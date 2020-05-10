2937ff86ba311e87d318495d0fade2bd7540f7dd
cicimen <egemengulkilik@gmail.com>
2020-05-10 20:52:16 +0300

    VisilabsInAppNotificationInstance class create

---
+++ VisilabsIOS/Classes/InAppNotification/VisilabsInAppNotificationInstance.swift
@@ -0,0 +1,16 @@
+//
+//  VisilabsInAppNotificationInstance.swift
+//  VisilabsIOS
+//
+//  Created by Egemen on 10.05.2020.
+//
+
+import Foundation
+
+
+
+
+class VisilabsInAppNotificationInstance {
+    
+    
+}
