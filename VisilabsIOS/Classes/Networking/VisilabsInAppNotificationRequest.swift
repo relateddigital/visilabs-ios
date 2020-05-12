133fbcc39e809c29f8c6504d7079b472381b9568
cicimen <egemengulkilik@gmail.com>
2020-05-13 00:38:44 +0300

    VisilabsInAppNotificationRequest completion type change to VisilabsInAppNotificationResponse

--- VisilabsIOS/Classes/Networking/VisilabsInAppNotificationRequest.swift
+++ VisilabsIOS/Classes/Networking/VisilabsInAppNotificationRequest.swift
@@ -10,7 +10,10 @@ import Foundation
 class VisilabsInAppNotificationRequest: VisilabsNetwork {
     
     //TODO: completion Any mi olmalı, yoksa AnyObject mi?
-    class func sendRequest(properties: [String : String], headers: [String : String], timeoutInterval: TimeInterval, completion: @escaping ([Any?]?) -> Void) {
-        
+    class func sendRequest(properties: [String : String], headers: [String : String], timeoutInterval: TimeInterval, completion: @escaping (VisilabsInAppNotificationResponse?) -> Void) {
+        var queryItems = [URLQueryItem]()
+        for property in properties{
+            queryItems.append(URLQueryItem(name: property.key, value: property.value))
+        }
     }
 }
