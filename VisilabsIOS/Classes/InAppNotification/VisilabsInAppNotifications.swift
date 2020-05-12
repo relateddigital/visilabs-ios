75c176ca7da14e1c88b245e5caf69936125857e6
cicimen <egemengulkilik@gmail.com>
2020-05-12 21:38:27 +0300

    VisilabsInAppNotificationType enum move to VisilabsInAppNotifications.swift

---
+++ VisilabsIOS/Classes/InAppNotification/VisilabsInAppNotifications.swift
@@ -0,0 +1,27 @@
+//
+//  VisilabsInAppNotifications.swift
+//  VisilabsIOS
+//
+//  Created by Egemen on 12.05.2020.
+//
+
+import Foundation
+import UIKit
+
+enum VisilabsInAppNotificationType : String {
+    case mini = "mini"
+    case full = "full"
+    case image_text_button = "image_text_button"
+    case full_image = "full_image"
+    case nps = "nps"
+    case image_button = "image_button"
+    case smile_rating = "smile_rating"
+    case unknown = "unknown"
+}
+
+
+
+
+
+
+
