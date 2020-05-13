df4833c69cc46feb7721acf3fb34957e200d6478
cicimen <egemengulkilik@gmail.com>
2020-05-13 11:22:10 +0300

    VisilabsBaseNotificationViewController implement

--- VisilabsIOS/Classes/InAppNotification/Controllers/VisilabsBaseNotificationViewController.swift
+++ VisilabsIOS/Classes/InAppNotification/Controllers/VisilabsBaseNotificationViewController.swift
@@ -20,4 +20,28 @@ protocol VisilabsNotificationViewControllerDelegate {
 
 class VisilabsBaseNotificationViewController: UIViewController {
 
+    var notification: VisilabsInAppNotification!
+    var delegate: VisilabsNotificationViewControllerDelegate?
+    var window: UIWindow?
+    var panStartPoint: CGPoint!
+    
+    convenience init(notification: VisilabsInAppNotification, nameOfClass: String) {
+        // avoiding `type(of: self)` as it doesn't work with Swift 4.0.3 compiler
+        // perhaps due to `self` not being fully constructed at this point
+        let bundle = Bundle(for: VisilabsBaseNotificationViewController.self)
+        self.init(nibName: nameOfClass, bundle: bundle)
+        self.notification = notification
+    }
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return .all
+    }
+
+    override var shouldAutorotate: Bool {
+        return true
+    }
+
+    func show(animated: Bool) {}
+    func hide(animated: Bool, completion: @escaping () -> Void) {}
+    
 }
