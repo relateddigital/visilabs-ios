867de5f9c1d3ea58a4048f278f38d8a64db777f9
cicimen <egemengulkilik@gmail.com>
2020-05-13 21:15:15 +0300

    VisilabsInAppNotificationRequest implementation complete

--- VisilabsIOS/Classes/Networking/VisilabsInAppNotificationRequest.swift
+++ VisilabsIOS/Classes/Networking/VisilabsInAppNotificationRequest.swift
@@ -10,10 +10,35 @@ import Foundation
 class VisilabsInAppNotificationRequest: VisilabsNetwork {
     
     //TODO: completion Any mi olmalı, yoksa AnyObject mi?
-    class func sendRequest(properties: [String : String], headers: [String : String], timeoutInterval: TimeInterval, completion: @escaping (VisilabsInAppNotificationResponse?) -> Void) {
+    class func sendRequest(properties: [String : String], headers: [String : String], timeoutInterval: TimeInterval, completion: @escaping ([Any]?) -> Void) {
         var queryItems = [URLQueryItem]()
         for property in properties{
             queryItems.append(URLQueryItem(name: property.key, value: property.value))
         }
+        
+        let responseParser: (Data) -> [Any]? = { data in
+            var response: Any? = nil
+            do {
+                response = try JSONSerialization.jsonObject(with: data, options: [])
+            } catch {
+                VisilabsLogger.warn(message: "exception decoding api data")
+            }
+            return response as? [Any]
+        }
+        
+        let resource = VisilabsNetwork.buildResource(endPoint: .action, method: .get, timeoutInterval: timeoutInterval, requestBody: nil, queryItems: queryItems, headers: headers, parse: responseParser )
+        
+        sendRequestHandler(resource: resource, completion: { result in completion(result) })
+        
+    }
+    
+    private class func sendRequestHandler(resource: VisilabsResource<[Any]>, completion: @escaping ([Any]?) -> Void) {
+        VisilabsNetwork.apiRequest(resource: resource,
+            failure: { (reason, data, response) in
+                VisilabsLogger.warn(message: "API request to \(resource.endPoint) has failed with reason \(reason)")
+                completion(nil)
+            }, success: { (result, response) in
+                completion(result)
+            })
     }
 }
