210a65c70a0d88c30228eb511a712343f9ac7863
cicimen <egemengulkilik@gmail.com>
2020-05-22 22:15:01 +0300

    InAppViewController add to example project

---
+++ Example/VisilabsIOS/InAppViewController.swift
@@ -0,0 +1,25 @@
+//
+//  InAppViewController.swift
+//  VisilabsIOS_Example
+//
+//  Created by Egemen on 22.05.2020.
+//  Copyright © 2020 CocoaPods. All rights reserved.
+//
+
+import Foundation
+
+
+import UIKit
+
+class InAppViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
