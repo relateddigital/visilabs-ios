5b2031ff9e15ca6f705706386e2abe4a17e43243
cicimen <egemengulkilik@gmail.com>
2020-05-22 21:59:30 +0300

    Example project EventViewController class add

---
+++ Example/VisilabsIOS/EventViewController.swift
@@ -0,0 +1,31 @@
+//
+//  EventViewController.swift
+//  VisilabsIOS_Example
+//
+//  Created by Egemen on 22.05.2020.
+//  Copyright © 2020 CocoaPods. All rights reserved.
+//
+
+import Foundation
+
+
+import UIKit
+import VisilabsIOS
+
+class EventViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //let props = [String : String]()
+        
+        //Visilabs2.callAPI().customEvent("test", properties: props)
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
