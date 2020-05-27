bcd30595229a025c157ae4b9b9c132135c48af45
cicimen <egemengulkilik@gmail.com>
2020-05-27 22:58:12 +0300

    VisilabsInAppNotificationInstance VisilabsInAppNotificationResponse convert to VisilabsInAppNotification

--- VisilabsIOS/Classes/InAppNotification/VisilabsInAppNotificationInstance.swift
+++ VisilabsIOS/Classes/InAppNotification/VisilabsInAppNotificationInstance.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 
+/*
 struct VisilabsInAppNotificationResponse {
     var inAppNotifications: [VisilabsInAppNotification]
 
@@ -14,6 +15,7 @@ struct VisilabsInAppNotificationResponse {
         inAppNotifications = []
     }
 }
+ */
 
 
 class VisilabsInAppNotificationInstance {
@@ -30,42 +32,40 @@ class VisilabsInAppNotificationInstance {
         }
     }
     
+    //TODO: lock kullanılmıyor. VisilabsInAppNotificationInstance da property olarak durmasına gerek var mı?
     required init(lock: VisilabsReadWriteLock) {
         self.lock = lock
         self.notificationsInstance = VisilabsInAppNotifications(lock: self.lock)
     }
     
     
-    func checkInAppNotification(properties: [String:String], visilabsUser: VisilabsUser, timeoutInterval: TimeInterval, completion: @escaping ((_ response: VisilabsInAppNotificationResponse?) -> Void)){
-        var visilabsInAppNotificationResponse = VisilabsInAppNotificationResponse()
+    func checkInAppNotification(properties: [String:String], visilabsUser: VisilabsUser, timeoutInterval: TimeInterval, completion: @escaping ((_ response: VisilabsInAppNotification?) -> Void)){
         let semaphore = DispatchSemaphore(value: 0)
         let headers = prepareHeaders(visilabsUser)
+        var notifications = [VisilabsInAppNotification]()
         
-        VisilabsInAppNotificationRequest.sendRequest(properties: properties, headers: headers, timeoutInterval: timeoutInterval, completion: { [weak self] visilabsInAppNotificationResult in
-            guard let self = self else {
-                return
-            }
-            guard let result = visilabsInAppNotificationResult else {
+        VisilabsInAppNotificationRequest.sendRequest(properties: properties, headers: headers, timeoutInterval: timeoutInterval, completion: { visilabsInAppNotificationResult in
+            guard let result = visilabsInAppNotificationResult else  {
                 semaphore.signal()
                 completion(nil)
                 return
             }
-            
-            //TODO: burada actiontype kontrolü de yapılabilir.
-            var parsedNotifications = [VisilabsInAppNotification]()
+
             for rawNotif in result{
                 if let actionData = rawNotif["actiondata"] as? [String : Any] {
-                    if let typeString = actionData["msg_type"] as? String, let type = VisilabsInAppNotificationType(rawValue: typeString), let notification = VisilabsInAppNotification(JSONObject: rawNotif) {
-                        parsedNotifications.append(notification)
+                    if let typeString = actionData["msg_type"] as? String, let _ = VisilabsInAppNotificationType(rawValue: typeString), let notification = VisilabsInAppNotification(JSONObject: rawNotif) {
+                        notifications.append(notification)
                     }
                 }
             }
-            
-            self.notificationsInstance.inAppNotifications = parsedNotifications
-            visilabsInAppNotificationResponse.inAppNotifications = parsedNotifications
             semaphore.signal()
         })
         _ = semaphore.wait(timeout: DispatchTime.distantFuture)
+        
+        VisilabsLogger.info(message: "in app notification check: \(notifications.count) found. actid's: \(notifications.map({String($0.actId)}).joined(separator: ","))")
+        
+        self.notificationsInstance.inAppNotification = notifications.first
+        completion(notifications.first)
     }
     
     private func prepareHeaders(_ visilabsUser: VisilabsUser) -> [String:String] {
