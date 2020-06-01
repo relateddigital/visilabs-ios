4d72c03f62e610b8044762d99379e51ffee7ddfd
cicimen <egemengulkilik@gmail.com>
2020-06-01 17:07:23 +0300

    FormHelper add to Example project

---
+++ Example/VisilabsIOS/Helpers/FormHelper.swift
@@ -0,0 +1,88 @@
+//
+//  AppDelegate.swift
+//  VisilabsIOS
+//
+//  Created by egemen@visilabs.com on 03/30/2020.
+//  Copyright (c) 2020 egemen@visilabs.com. All rights reserved.
+//
+
+import Eureka
+
+enum RowType {
+    case textRow
+    case urlRow
+    case pickerInputRow
+    case switchRow
+}
+
+class FormRow <Element> {
+    var rowType: RowType
+    var tag: String
+    var title: String?
+    var placeholder: String?
+    var options: [Element]?
+    var value: Element?
+    var rules = [BaseRuleType]()
+    init(rowType: RowType, tag: String, title: String?, placeholder: String?, options: [Element]?, value: Element?){
+        self.rowType = rowType
+        self.tag = tag
+        self.title = title
+        self.placeholder = placeholder
+        self.options = options
+        self.value = value
+    }
+}
+
+class FormSection {
+    var header: String?
+    var formRows = [FormRow<Any>]()
+}
+
+
+
+class FormHelper{
+
+    func createForm(formSections: [FormSection]) -> Form {
+        LabelRow.defaultCellUpdate = { cell, row in
+            cell.contentView.backgroundColor = .red
+            cell.textLabel?.textColor = .white
+            cell.textLabel?.font = UIFont.boldSystemFont(ofSize: 13)
+            cell.textLabel?.textAlignment = .right
+        }
+        
+        let form = Form()
+        
+        for formSection in formSections{
+            let section = Section(formSection.header)
+            
+            for formRow in formSection.formRows {
+                var baseRow: BaseRow?
+                if formRow.rowType == .textRow {
+                    baseRow = TextRow(formRow.tag) { (row) in
+                        row.placeholder = formRow.placeholder
+                        row.value = formRow.value as? String
+                    }
+                }
+                if formRow.rowType == .urlRow {
+                    baseRow = URLRow(formRow.tag) { (row) in
+                        row.placeholder = formRow.placeholder
+                        row.value = formRow.value as? URL
+                    }
+                }
+                
+                baseRow?.title = formRow.title
+                section.append(baseRow!)
+                
+            
+            
+                
+            }
+            form.append(section)
+        }
+        
+        
+        return form
+        
+    }
+}
+
