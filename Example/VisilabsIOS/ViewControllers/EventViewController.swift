36ddb479202484dfc8d28a6d376f9ae78038b951
cicimen <egemengulkilik@gmail.com>
2020-06-03 20:33:28 +0300

    Example project some common events add

--- Example/VisilabsIOS/ViewControllers/EventViewController.swift
+++ Example/VisilabsIOS/ViewControllers/EventViewController.swift
@@ -7,31 +7,111 @@
 //
 
 import Foundation
+import Eureka
 
 
 import UIKit
 import VisilabsIOS
 
-class EventViewController: UIViewController {
+enum VisilabsEventType {
+    case login
+    case pageView
+    case productView
+
+    case mini
+    case full
+}
+
+class EventViewController: FormViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        var properties = [String:String]()
-        properties["OM.clist"] = "123"
-        properties["OM.exVisitorID"] = "umut@visilabs.com"
-        properties["OM.sys.AppID"] = "EuromsgTest"
-        Visilabs2.callAPI().customEvent("Test", properties: properties)
-        print(properties)
+        initializeForm()
     }
+    
+    private func initializeForm() {
+    
+        LabelRow.defaultCellUpdate = { cell, row in
+            cell.contentView.backgroundColor = .red
+            cell.textLabel?.textColor = .white
+            cell.textLabel?.font = UIFont.boldSystemFont(ofSize: 13)
+            cell.textLabel?.textAlignment = .right
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        }
+
+        form +++
+        
+        Section("Common Events")
+            
+            <<< ButtonRow() {
+                $0.title = "Login"
+            }
+            .onCellSelection { cell, row in
+                self.customEvent(.login)
+            }
+
+            <<< ButtonRow() {
+                $0.title = "Page View"
+            }
+            .onCellSelection { cell, row in
+                self.customEvent(.pageView)
+            }
+            
+            <<< ButtonRow() {
+                $0.title = "Product View"
+            }
+            .onCellSelection { cell, row in
+                self.customEvent(.productView)
+            }
+        
+        +++ Section("In App Notification Events")
+            
+            <<< ButtonRow() {
+                $0.title = "Mini"
+            }
+            .onCellSelection { cell, row in
+                self.customEvent(.mini)
+            }
     }
-    @IBAction func geri(_ sender: Any) {
-        if let vc = storyboard?.instantiateViewController(withIdentifier: "homeview") {
-           self.view.window?.rootViewController = vc
+    
+    private func customEvent(_ eventType: VisilabsEventType){
+        switch eventType {
+            case .login:
+                var properties = [String:String]()
+                properties["OM.sys.TokenID"] = "Token ID to use for push messages"
+                properties["OM.sys.AppID"] = "App ID to use for push messages"
+                Visilabs.callAPI().login(exVisitorId: "userId", properties: properties)
+                return
+            case .pageView:
+                Visilabs.callAPI().customEvent("Page Name", properties: [String:String]())
+                return
+            case .productView:
+                var properties = [String:String]()
+                properties["OM.pv"] = "Product Code"
+                properties["OM.pn"] = "Product Name"
+                properties["OM.ppr"] = "Product Price"
+                properties["OM.pv.1"] = "Product Brand"
+                properties["OM.ppr"] = "Product Price"
+                properties["OM.inv"] = "Number of items in stock"
+                Visilabs.callAPI().customEvent("Product View", properties: properties)
+                return
+            
+            
+            case .mini:
+                var properties = [String:String]()
+                properties["OM.inapptype"] = "mini"
+                Visilabs.callAPI().customEvent("InAppTest", properties: properties)
+                return
+            case .full:
+                var properties = [String:String]()
+                properties["OM.inapptype"] = "full"
+                Visilabs.callAPI().customEvent("InAppTest", properties: properties)
+                return
+            
+            default:
+                return
         }
     }
     
+
 }
