5ab2a6b685c02d41b015946c8cccdba1f7511d77
cicimen <egemengulkilik@gmail.com>
2020-06-03 20:31:52 +0300

    Example project FormHelper refactor

--- Example/VisilabsIOS/Helpers/FormHelper.swift
+++ Example/VisilabsIOS/Helpers/FormHelper.swift
@@ -14,32 +14,41 @@ enum RowType {
     case pickerInputRow
     case switchRow
     case buttonRow
+    case colorRow
 }
 
-class FormRow <Element> {
+
+
+
+class FormRow {
     var rowType: RowType
+    var rowValueType: Any.Type
     var tag: String
     var title: String?
     var placeholder: String?
-    var options: [Element]?
-    var value: Element?
+    var options: [Any]
+    var value: Any?
     var rules = [BaseRuleType]()
-    init(rowType: RowType, tag: String, title: String?, placeholder: String?, options: [Element]?, value: Element?){
+    init(rowType: RowType, rowValueType: Any.Type, tag: String, title: String?, placeholder: String? = nil, options: [Any] = [], value: Any? = nil, rules: [BaseRuleType]? = nil){
         self.rowType = rowType
+        self.rowValueType = rowValueType
         self.tag = tag
         self.title = title
         self.placeholder = placeholder
         self.options = options
         self.value = value
+        if let r = rules {
+            self.rules = r
+        }
     }
 }
 
 class FormSection {
     var header: String?
-    var formRows: [FormRow<Any>]
-    init(header: String?, formRows: [FormRow<Any>]){
+    var sectionRows: [FormRow]
+    init(_ header: String?, _ sectionRows: [FormRow]){
         self.header = header
-        self.formRows = formRows
+        self.sectionRows = sectionRows
     }
     
 }
@@ -61,7 +70,7 @@ class FormHelper{
         for formSection in formSections{
             let section = Section(formSection.header)
             
-            for formRow in formSection.formRows {
+            for formRow in formSection.sectionRows {
                 var baseRow: BaseRow?
                 if formRow.rowType == .textRow {
                     baseRow = TextRow(formRow.tag) { (row) in
@@ -75,7 +84,18 @@ class FormHelper{
                         row.value = formRow.value as? URL
                     }
                 }
-                
+                if formRow.rowType == .pickerInputRow {
+                    if formRow.rowValueType == String.self{
+                        baseRow = PickerInputRow<String>(formRow.tag) { (row) in
+                            row.value = formRow.value as? String
+                            for option in formRow.options{
+                                if let opt = option as? String{
+                                    row.options.append(opt)
+                                }
+                            }
+                        }
+                    }
+                }
                 baseRow?.title = formRow.title
                 section.append(baseRow!)
                 
