1426f7c516ce5c1613587323992c7baa6642d7c4
cicimen <egemengulkilik@gmail.com>
2020-06-12 10:11:53 +0300

    UIColor hex constructor add

--- VisilabsIOS/Classes/InAppNotification/Controllers/VisilabsBaseNotificationViewController.swift
+++ VisilabsIOS/Classes/InAppNotification/Controllers/VisilabsBaseNotificationViewController.swift
@@ -62,6 +62,26 @@ extension UIColor {
         let blue    = CGFloat( MPHex & 0x000000FF       ) / divisor
         self.init(red: red, green: green, blue: blue, alpha: alpha)
     }
+    
+    public convenience init(hex: String, alpha: CGFloat = 1.0) {
+      var cString:String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+
+      if (cString.hasPrefix("#")) { cString.removeFirst() }
+
+      if ((cString.count) != 6) {
+        self.init(hex: "ff0000") // return red color for wrong hex input
+        return
+      }
+
+      var rgbValue: UInt64 = 0
+      Scanner(string: cString).scanHexInt64(&rgbValue)
+
+      self.init(red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+                green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+                blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+                alpha: alpha)
+    }
+    
 
     /**
      Add two colors together
