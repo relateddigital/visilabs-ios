6e1631330993bb2b8a7f2a7df3b1017f9a2cc56a
cicimen <egemengulkilik@gmail.com>
2020-06-12 07:53:24 +0300

    VisilabsCaptureQuality and VisilabsTrackingMode enums add

---
+++ VisilabsIOS/Classes/InAppNotification/Models/VisilabsCaptureQuality.swift
@@ -0,0 +1,31 @@
+//
+//  VisilabsCaptureQuality.swift
+//  VisilabsIOS
+//
+//  Created by Egemen on 12.06.2020.
+//
+
+public enum VisilabsCaptureQuality {
+    case `default`
+    case low
+    case medium
+    case high
+
+    var imageScale: CGFloat {
+        switch self {
+        case .default, .high:
+            return 0
+        case .low, .medium:
+            return  1
+        }
+    }
+
+    var interpolationQuality: CGInterpolationQuality {
+        switch self {
+        case .default, .low:
+            return .none
+        case .medium, .high:
+            return .default
+        }
+    }
+}
