2096625d7ae35441234436874e2614b0908d2893
cicimen <egemengulkilik@gmail.com>
2020-06-12 15:44:16 +0300

    VisilabsFullNotificationViewController implementation complete

--- VisilabsIOS/Classes/InAppNotification/Controllers/VisilabsFullNotificationViewController.swift
+++ VisilabsIOS/Classes/InAppNotification/Controllers/VisilabsFullNotificationViewController.swift
@@ -17,69 +17,172 @@ class VisilabsFullNotificationViewController: VisilabsBaseNotificationViewContro
     
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var bodyLabel: UILabel!
+    @IBOutlet weak var inAppButton: UIButton!
+    @IBOutlet weak var closeButton: UIButton!
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    @IBOutlet weak var viewMask: UIView!
+    @IBOutlet weak var fadingView: FadingView!
+    @IBOutlet weak var bottomImageSpacing: NSLayoutConstraint!
     
-    var isDismissing = false
-    var canPan = true
-    var position: CGPoint!
-
     convenience init(notification: VisilabsInAppNotification) {
-        self.init(notification: notification, nameOfClass: String(describing: VisilabsMiniNotificationViewController.self))
+        self.init(notification: notification, nameOfClass: String(describing: VisilabsFullNotificationViewController.notificationXibToLoad()))
     }
+    
+    static func notificationXibToLoad() -> String {
+        var xibName = String(describing: VisilabsFullNotificationViewController.self)
+        guard VisilabsInstance.sharedUIApplication() != nil else {
+            return xibName
+        }
+        if UIDevice.current.userInterfaceIdiom == UIUserInterfaceIdiom.phone {
+            if UIDevice.current.orientation.isLandscape {
+                xibName += "~iphonelandscape"
+            } else {
+                xibName += "~iphoneportrait"
+            }
+        } else {
+            xibName += "~ipad"
+        }
 
+        return xibName
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        titleLabel.text = notification.messageTitle
-        if let image = notification.image {
-            imageView.image = UIImage(data: image)
+        if let notificationImage = notification.image, let image = UIImage(data: notificationImage, scale: 2) {
+            imageView.image = image
+            if let width = imageView.image?.size.width, width / UIScreen.main.bounds.width <= 0.6, let height = imageView.image?.size.height,
+                height / UIScreen.main.bounds.height <= 0.3 {
+                imageView.contentMode = UIView.ContentMode.center
+            }
+        } else {
+            VisilabsLogger.error(message: "notification image failed to load from data")
         }
 
-        //TODO: bunları default set ediyorum doğru mudur?
-        view.backgroundColor = UIColor(MPHex: 3858759680)
-        titleLabel.textColor = UIColor(MPHex: 4294967295)
-        imageView.tintColor = UIColor(MPHex: 4294967295)
+        /*
+        if fullNotification.messageTitle == nil || fullNotification.messageBody == nil {
+            NSLayoutConstraint(item: titleLabel!,
+                               attribute: NSLayoutConstraint.Attribute.height,
+                               relatedBy: NSLayoutConstraint.Relation.equal,
+                               toItem: nil,
+                               attribute: NSLayoutConstraint.Attribute.notAnAttribute,
+                               multiplier: 1,
+                               constant: 0).isActive = true
+            NSLayoutConstraint(item: bodyLabel!,
+                               attribute: NSLayoutConstraint.Attribute.height,
+                               relatedBy: NSLayoutConstraint.Relation.equal,
+                               toItem: nil,
+                               attribute: NSLayoutConstraint.Attribute.notAnAttribute,
+                               multiplier: 1,
+                               constant: 0).isActive = true
+        } else {
+            titleLabel.text = fullNotification.messageTitle
+            bodyLabel.text = fullNotification.messageBody
+        }
+ */
+
         
-        //view.backgroundColor = UIColor(MPHex: miniNotification.backgroundColor)
-        //bodyLabel.textColor = UIColor(MPHex: miniNotification.bodyColor)
-        imageView.image = imageView.image?.withRenderingMode(.alwaysTemplate)
-        //imageView.tintColor = UIColor(MPHex: miniNotification.imageTintColor)
+        titleLabel.text = fullNotification.messageTitle
+        bodyLabel.text = fullNotification.messageBody
+        
+        if let bgColor = fullNotification.backGroundColor {
+            viewMask.backgroundColor = UIColor(hex: bgColor, alpha: 0.8)
+        }else{
+            viewMask.backgroundColor = UIColor(hex: "#000000", alpha: 0.8)
+        }
+        
+        if let tColor = fullNotification.messageTitleColor {
+            titleLabel.textColor = UIColor(hex: tColor, alpha: 1)
+        }else{
+            titleLabel.textColor = UIColor(hex: "#FFFFFF", alpha: 1)
+        }
+        
+        if let bColor = fullNotification.messageBodyColor {
+            bodyLabel.textColor = UIColor(hex: bColor, alpha: 1)
+        }else{
+            bodyLabel.textColor = UIColor(hex: "#FFFFFF", alpha: 1)
+        }
 
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(didTap(gesture:)))
-        tapGesture.numberOfTapsRequired = 1
-        window?.addGestureRecognizer(tapGesture)
+        let origImage = closeButton.image(for: UIControl.State.normal)
+        let tintedImage = origImage?.withRenderingMode(UIImage.RenderingMode.alwaysTemplate)
+        closeButton.setImage(tintedImage, for: UIControl.State.normal)
+        closeButton.tintColor = UIColor(hex: "#FFFFFF", alpha: 1)
+        closeButton.imageView?.contentMode = UIView.ContentMode.scaleAspectFit
 
-        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(didPan(gesture:)))
-        window?.addGestureRecognizer(panGesture)
+        
+        setupButtonView(buttonView: inAppButton)
+        
+        /*
+        if takeoverNotification.buttons.count >= 1 {
+            setupButtonView(buttonView: firstButton, buttonModel: takeoverNotification.buttons[0], index: 0)
+            if takeoverNotification.buttons.count == 2 {
+                setupButtonView(buttonView: secondButton, buttonModel: takeoverNotification.buttons[1], index: 1)
+            } else {
+                NSLayoutConstraint(item: secondButtonContainer!,
+                                   attribute: NSLayoutConstraint.Attribute.width,
+                                   relatedBy: NSLayoutConstraint.Relation.equal,
+                                   toItem: nil,
+                                   attribute: NSLayoutConstraint.Attribute.notAnAttribute,
+                                   multiplier: 1,
+                                   constant: 0).isActive = true
+            }
+        }
+
+
+        if !takeoverNotification.shouldFadeImage {
+            if bottomImageSpacing != nil {
+                bottomImageSpacing.constant = 30
+            }
+            fadingView.layer.mask = nil
+        }
+  */
+
+        if UIDevice.current.userInterfaceIdiom == UIUserInterfaceIdiom.pad {
+            
+            if let bgColor = fullNotification.backGroundColor {
+                self.view.backgroundColor = UIColor(hex: bgColor, alpha: 0.6)
+            }else{
+                self.view.backgroundColor = UIColor(hex: "#000000", alpha: 0.6)
+            }
+            
+            //self.view.backgroundColor = UIColor(hex: "#000000", alpha: 0.8)
+            //self.view.backgroundColor = self.view.backgroundColor?.withAlphaComponent(0.8)
+            viewMask.clipsToBounds = true
+            viewMask.layer.cornerRadius = 6
+        }
+
     }
-
+    
+    func setupButtonView(buttonView: UIButton) {
+        buttonView.setTitle(fullNotification.buttonText, for: UIControl.State.normal)
+        buttonView.titleLabel?.adjustsFontSizeToFitWidth = true
+        buttonView.layer.cornerRadius = 20
+        buttonView.layer.borderWidth = 2
+        
+        var buttonColor = UIColor.black
+        if let bColor = fullNotification.buttonColor{
+            buttonColor = UIColor(hex: bColor)
+        }
+        
+        var buttonTextColor = UIColor.white
+        if let bColor = fullNotification.buttonTextColor{
+            buttonTextColor = UIColor(hex: bColor)
+        }
+        
+        buttonView.setTitleColor(buttonTextColor, for: UIControl.State.normal)
+        buttonView.setTitleColor(buttonTextColor, for: UIControl.State.highlighted)
+        buttonView.setTitleColor(buttonTextColor, for: UIControl.State.selected)
+        buttonView.layer.borderColor = buttonView.currentTitleColor.cgColor
+        buttonView.backgroundColor = buttonColor
+        buttonView.addTarget(self, action: #selector(buttonTapped(_:)), for: UIControl.Event.touchUpInside)
+        buttonView.tag = 0
+    }
+    
+    
     override func show(animated: Bool) {
         guard let sharedUIApplication = VisilabsInstance.sharedUIApplication() else {
             return
-        }
-        canPan = false
-        var bounds: CGRect
-        if #available(iOS 13.0, *) {
-            let windowScene = sharedUIApplication
-                           .connectedScenes
-                           .filter { $0.activationState == .foregroundActive }
-                           .first
-            guard let scene = windowScene as? UIWindowScene else { return }
-            bounds = scene.coordinateSpace.bounds
-        } else {
-            bounds = UIScreen.main.bounds
-        }
-        let frame: CGRect
-        if sharedUIApplication.statusBarOrientation.isPortrait
-            && UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiom.phone {
-            frame = CGRect(x: VisilabsInAppNotificationsConstants.miniSidePadding,
-                           y: bounds.size.height,
-                           width: bounds.size.width - (VisilabsInAppNotificationsConstants.miniSidePadding * 2),
-                           height: VisilabsInAppNotificationsConstants.miniInAppHeight)
-        } else { // Is iPad or Landscape mode
-            frame = CGRect(x: bounds.size.width / 4,
-                           y: bounds.size.height,
-                           width: bounds.size.width / 2,
-                           height: VisilabsInAppNotificationsConstants.miniInAppHeight)
         }
         if #available(iOS 13.0, *) {
             let windowScene = sharedUIApplication
@@ -87,84 +190,107 @@ class VisilabsFullNotificationViewController: VisilabsBaseNotificationViewContro
                 .filter { $0.activationState == .foregroundActive }
                 .first
             if let windowScene = windowScene as? UIWindowScene {
-                window = UIWindow(frame: frame)
+                window = UIWindow(frame: windowScene.coordinateSpace.bounds)
                 window?.windowScene = windowScene
             }
         } else {
-            window = UIWindow(frame: frame)
+            window = UIWindow(frame: CGRect(x: 0,
+                                            y: 0,
+                                            width: UIScreen.main.bounds.size.width,
+                                            height: UIScreen.main.bounds.size.height))
         }
-
         if let window = window {
+            window.alpha = 0
             window.windowLevel = UIWindow.Level.alert
-            window.clipsToBounds = true
             window.rootViewController = self
-            window.layer.cornerRadius = 6
-            
-            //TODO: bunları default set ediyorum doğru mudur?
-            window.layer.borderColor = UIColor(MPHex: 4294967295).cgColor
-            //window.layer.borderColor = UIColor(MPHex: miniNotification.borderColor).cgColor
-            window.layer.borderWidth = 1
             window.isHidden = false
         }
 
-        let duration = animated ? 0.1 : 0
+        let duration = animated ? 0.25 : 0
         UIView.animate(withDuration: duration, animations: {
-            self.window?.frame.origin.y -= (VisilabsInAppNotificationsConstants.miniInAppHeight + VisilabsInAppNotificationsConstants.miniBottomPadding)
-            self.canPan = true
+            self.window?.alpha = 1
             }, completion: { _ in
-                self.position = self.window?.layer.position
         })
     }
 
     override func hide(animated: Bool, completion: @escaping () -> Void) {
-        if !isDismissing {
-            canPan = false
-            isDismissing = true
-            let duration = animated ? 0.5 : 0
-            UIView.animate(withDuration: duration, animations: {
-                self.window?.frame.origin.y += (VisilabsInAppNotificationsConstants.miniInAppHeight + VisilabsInAppNotificationsConstants.miniBottomPadding)
-                }, completion: { _ in
-                    self.window?.isHidden = true
-                    self.window?.removeFromSuperview()
-                    self.window = nil
-                    completion()
-            })
-        }
+        let duration = animated ? 0.25 : 0
+        UIView.animate(withDuration: duration, animations: {
+            self.window?.alpha = 0
+            }, completion: { _ in
+                self.window?.isHidden = true
+                self.window?.removeFromSuperview()
+                self.window = nil
+                completion()
+        })
     }
 
-    @objc func didTap(gesture: UITapGestureRecognizer) {
-        
+    
+    //TODO: burada additionalTrackingProperties kısmında aksiyon id'si gönderilebilir.
+    @objc func buttonTapped(_ sender: AnyObject) {
+        delegate?.notificationShouldDismiss(controller: self,
+                                            callToActionURL: fullNotification.callToActionUrl,
+                                            shouldTrack: true,
+                                            additionalTrackingProperties: nil)
+    }
+    
+    
+    
+    
+    @IBAction func tappedClose(_ sender: Any) {
+        delegate?.notificationShouldDismiss(controller: self,
+        callToActionURL: nil,
+        shouldTrack: false,
+        additionalTrackingProperties: nil)
     }
 
-    @objc func didPan(gesture: UIPanGestureRecognizer) {
-        
+    override var shouldAutorotate: Bool {
+        return false
     }
 
-    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
-        guard VisilabsInstance.sharedUIApplication() != nil else {
-            return
-        }
-        super.viewWillTransition(to: size, with: coordinator)
-        coordinator.animate(alongsideTransition: { (ctx) in
-            let frame: CGRect
-            if  UIDevice.current.orientation.isPortrait
-                && UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiom.phone {
-                frame = CGRect(x: VisilabsInAppNotificationsConstants.miniSidePadding,
-                               y: UIScreen.main.bounds.size.height -
-                                (VisilabsInAppNotificationsConstants.miniInAppHeight + VisilabsInAppNotificationsConstants.miniBottomPadding),
-                               width: UIScreen.main.bounds.size.width -
-                                (VisilabsInAppNotificationsConstants.miniSidePadding * 2),
-                               height: VisilabsInAppNotificationsConstants.miniInAppHeight)
-            } else { // Is iPad or Landscape mode
-                frame = CGRect(x: UIScreen.main.bounds.size.width / 4,
-                               y: UIScreen.main.bounds.size.height -
-                                (VisilabsInAppNotificationsConstants.miniInAppHeight + VisilabsInAppNotificationsConstants.miniBottomPadding),
-                               width: UIScreen.main.bounds.size.width / 2,
-                               height: VisilabsInAppNotificationsConstants.miniInAppHeight)
-            }
-            self.window?.frame = frame
 
-            }, completion: nil)
+}
+
+class FadingView: UIView {
+    var gradientMask: CAGradientLayer!
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        gradientMask = CAGradientLayer()
+        layer.mask = gradientMask
+        gradientMask.colors = [UIColor.black.cgColor, UIColor.black.cgColor, UIColor.clear.cgColor, UIColor.clear.cgColor]
+        gradientMask.locations = [0, 0.4, 0.9, 1]
+        gradientMask.startPoint = CGPoint(x: 0, y: 0)
+        gradientMask.endPoint = CGPoint(x: 0, y: 1)
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        gradientMask.frame = bounds
     }
 }
 
+class InAppButtonView: UIButton {
+    var origColor: UIColor?
+    var wasCalled = false
+    let overlayColor = UIColor(MPHex: 0x33868686)
+    override var isHighlighted: Bool {
+        didSet {
+            switch isHighlighted {
+            case true:
+                if !wasCalled {
+                    origColor = backgroundColor
+                    if origColor == UIColor(red: 0, green: 0, blue: 0, alpha: 0) {
+                        backgroundColor = overlayColor
+                    } else {
+                        backgroundColor = backgroundColor?.add(overlay: overlayColor)
+                    }
+                    wasCalled = true
+                }
+            case false:
+                backgroundColor = origColor
+                wasCalled = false
+            }
+        }
+    }
+}
