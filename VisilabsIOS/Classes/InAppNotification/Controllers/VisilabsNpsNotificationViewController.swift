b6ebfe3a9783a2259d73d4d61f3eced67e20cfa6
cicimen <egemengulkilik@gmail.com>
2020-06-16 16:35:45 +0300

    VisilabsNpsNotificationViewController class add

---
+++ VisilabsIOS/Classes/InAppNotification/Controllers/VisilabsNpsNotificationViewController.swift
@@ -0,0 +1,12 @@
+//
+//  VisilabsNpsNotificationViewController.swift
+//  VisilabsIOS
+//
+//  Created by Egemen on 16.06.2020.
+//
+
+import Foundation
+
+class VisilabsNpsNotificationViewController: UIViewController {
+    
+}
