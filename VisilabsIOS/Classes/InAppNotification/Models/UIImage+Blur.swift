8209f2676ce24a65ef3378fd76e7161cad568558
cicimen <egemengulkilik@gmail.com>
2020-06-17 10:00:59 +0300

    UIImage extension resized add

--- VisilabsIOS/Classes/InAppNotification/Models/UIImage+Blur.swift
+++ VisilabsIOS/Classes/InAppNotification/Models/UIImage+Blur.swift
@@ -24,4 +24,12 @@ public extension UIImage {
             UIImage(cgImage: $0, scale: scale, orientation: imageOrientation)
         }
     }
+    func resized(withPercentage percentage: CGFloat, isOpaque: Bool = false) -> UIImage? {
+        let canvas = CGSize(width: size.width * percentage, height: size.height * percentage)
+        let format = imageRendererFormat
+        format.opaque = isOpaque
+        return UIGraphicsImageRenderer(size: canvas, format: format).image {
+            _ in draw(in: CGRect(origin: .zero, size: canvas))
+        }
+    }
 }
