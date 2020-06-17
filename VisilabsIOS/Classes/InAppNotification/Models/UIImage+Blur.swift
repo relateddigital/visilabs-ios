6d6b33b967922f2b616f6ce08a5010c64f5d78c7
cicimen <egemengulkilik@gmail.com>
2020-06-17 18:15:39 +0300

    UIImage extension init? UIImage.RenderingMode add

--- VisilabsIOS/Classes/InAppNotification/Models/UIImage+Blur.swift
+++ VisilabsIOS/Classes/InAppNotification/Models/UIImage+Blur.swift
@@ -32,4 +32,35 @@ public extension UIImage {
             _ in draw(in: CGRect(origin: .zero, size: canvas))
         }
     }
+    
+    convenience init?(systemItem sysItem: UIBarButtonItem.SystemItem, renderingMode:UIImage.RenderingMode = .automatic) {
+        guard let sysImage = UIImage.imageFromSystemItem(sysItem, renderingMode: renderingMode)?.cgImage else {
+            return nil
+        }
+        self.init(cgImage: sysImage)
+    }
+
+    private class func imageFromSystemItem(_ systemItem: UIBarButtonItem.SystemItem, renderingMode:UIImage.RenderingMode = .automatic) -> UIImage? {
+
+        let tempItem = UIBarButtonItem(barButtonSystemItem: systemItem, target: nil, action: nil)
+
+        // add to toolbar and render it
+        let bar = UIToolbar()
+        bar.setItems([tempItem], animated: false)
+        bar.snapshotView(afterScreenUpdates: true)
+
+        // got image from real uibutton
+        let itemView = tempItem.value(forKey: "view") as! UIView
+
+        for view in itemView.subviews {
+            if view is UIButton {
+                let button = view as! UIButton
+                let image = button.imageView!.image!
+                image.withRenderingMode(renderingMode)
+                return image
+            }
+        }
+
+        return nil
+    }
 }
