0dd65932fd8cd851fe32e14dce41864041a95328
cicimen <egemengulkilik@gmail.com>
2020-06-17 15:23:58 +0300

    UIColor extension init convert to optional

--- VisilabsIOS/Classes/InAppNotification/Controllers/VisilabsBaseNotificationViewController.swift
+++ VisilabsIOS/Classes/InAppNotification/Controllers/VisilabsBaseNotificationViewController.swift
@@ -63,20 +63,23 @@ extension UIColor {
         self.init(red: red, green: green, blue: blue, alpha: alpha)
     }
     
-    public convenience init(hex: String, alpha: CGFloat = 1.0) {
-      var cString:String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+    public convenience init?(hex: String?, alpha: CGFloat = 1.0) {
+        
+        guard let hexString = hex else {
+            return nil
+        }
+        var cString: String = hexString.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
 
-      if (cString.hasPrefix("#")) { cString.removeFirst() }
+        if (cString.hasPrefix("#")) { cString.removeFirst() }
 
-      if ((cString.count) != 6) {
-        self.init(hex: "000000") // return black color for wrong hex input
-        return
-      }
+        if ((cString.count) != 6) {
+            return nil
+        }
 
-      var rgbValue: UInt64 = 0
-      Scanner(string: cString).scanHexInt64(&rgbValue)
+        var rgbValue: UInt64 = 0
+        Scanner(string: cString).scanHexInt64(&rgbValue)
 
-      self.init(red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+        self.init(red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
                 green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
                 blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
                 alpha: alpha)
