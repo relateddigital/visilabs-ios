6e8c2d43081a3f0bc5223f622a4e74077103b763
cicimen <egemengulkilik@gmail.com>
2020-06-22 16:42:39 +0300

    example project GeofenceViewController addd

---
+++ Example/VisilabsIOS/ViewControllers/GeofenceViewController.swift
@@ -0,0 +1,35 @@
+//
+//  GeofenceViewController.swift
+//  VisilabsIOS_Example
+//
+//  Created by Egemen on 22.06.2020.
+//  Copyright © 2020 CocoaPods. All rights reserved.
+//
+
+import Foundation
+import Eureka
+import UIKit
+import VisilabsIOS
+
+
+class GeofenceViewController: FormViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        initializeForm()
+    }
+    
+    
+    
+    
+    private func initializeForm() {
+    
+
+        form +++
+            
+        Section("Geofence Server Checks")
+        
+    }
+
+}
+
