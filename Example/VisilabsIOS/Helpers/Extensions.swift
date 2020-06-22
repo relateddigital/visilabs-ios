c90bcfce7d6b4137e37f5c7139611d8b81edc817
cicimen <egemengulkilik@gmail.com>
2020-06-22 15:38:33 +0300

    Example project UIColor extension hex init add

--- Example/VisilabsIOS/Helpers/Extensions.swift
+++ Example/VisilabsIOS/Helpers/Extensions.swift
@@ -6,4 +6,30 @@
 //  Copyright © 2020 CocoaPods. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension UIColor {
+
+    convenience init?(hex: String?, alpha: CGFloat = 1.0) {
+        
+        guard let hexString = hex else {
+            return nil
+        }
+        var cString: String = hexString.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+
+        if (cString.hasPrefix("#")) { cString.removeFirst() }
+
+        if ((cString.count) != 6) {
+            return nil
+        }
+
+        var rgbValue: UInt64 = 0
+        Scanner(string: cString).scanHexInt64(&rgbValue)
+
+        self.init(red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+                green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+                blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+                alpha: alpha)
+    }
+    
+}
