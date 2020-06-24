bca2f1f835544059c06425fe487b8ece83fe4014
cicimen <egemengulkilik@gmail.com>
2020-06-24 18:19:53 +0300

    example project EventViewController all event types implement

--- Example/VisilabsIOS/ViewControllers/EventViewController.swift
+++ Example/VisilabsIOS/ViewControllers/EventViewController.swift
@@ -18,6 +18,15 @@ enum VisilabsEventType : String, CaseIterable {
     case signUp  = "Sign Up"
     case pageView = "Page View"
     case productView = "Product View"
+    case productAddToCart = "Product Add to Cart"
+    case productPurchase = "Product Purchase"
+    case productCategoryPageView = "Product Category Page View"
+    case inAppSearch = "In App Search"
+    case bannerClick = "Banner Click"
+    case addToFavourites = "Add to Favourites"
+    case removeFromFavourites = "Remove from Favourites"
+    case sendingCampaignParameters = "Sending Campaign Parameters"
+    case pushMessage = "Push Message"
 }
 
 class EventViewController: FormViewController {
@@ -86,15 +95,14 @@ class EventViewController: FormViewController {
     
     
     private func customEvent(_ eventType: VisilabsEventType){
+        var properties = [String:String]()
         switch eventType {
         case .login:
-            var properties = [String:String]()
             properties["OM.sys.TokenID"] = "Token ID to use for push messages"
             properties["OM.sys.AppID"] = "App ID to use for push messages"
             Visilabs.callAPI().login(exVisitorId: "userId", properties: properties)
             return
         case .signUp:
-            var properties = [String:String]()
             properties["OM.sys.TokenID"] = "Token ID to use for push messages"
             properties["OM.sys.AppID"] = "App ID to use for push messages"
             Visilabs.callAPI().signUp(exVisitorId: "userId", properties: properties)
@@ -103,7 +111,6 @@ class EventViewController: FormViewController {
             Visilabs.callAPI().customEvent("Page Name", properties: [String:String]())
             return
         case .productView:
-            var properties = [String:String]()
             properties["OM.pv"] = "Product Code"
             properties["OM.pn"] = "Product Name"
             properties["OM.ppr"] = "Product Price"
@@ -112,7 +119,58 @@ class EventViewController: FormViewController {
             properties["OM.inv"] = "Number of items in stock"
             Visilabs.callAPI().customEvent("Product View", properties: properties)
             return
+        case .productAddToCart:
+            properties["OM.pbid"] = "Basket ID"
+            properties["OM.pb"] = "Product1 Code;Product2 Code"
+            properties["OM.pu"] = "Product1 Quantity;Product2 Quantity"
+            properties["OM.ppr"] = "Product1 Price*Product1 Quantity;Product2 Price*Product2 Quantity"
+            Visilabs.callAPI().customEvent("Cart", properties: properties)
+            return
+        case .productPurchase:
+            properties["OM.tid"] = "Order ID"
+            properties["OM.pp"] = "Product1 Code;Product2 Code"
+            properties["OM.pu"] = "Product1 Quantity;Product2 Quantity"
+            properties["OM.ppr"] = "Product1 Price*Product1 Quantity;Product2 Price*Product2 Quantity"
+            Visilabs.callAPI().customEvent("Purchase", properties: properties)
+            return
+        case .productCategoryPageView:
+            properties["OM.clist"] = "Category Code/Category ID"
+            Visilabs.callAPI().customEvent("Category View", properties: properties)
+            return
+        case .inAppSearch:
+            properties["OM.oss"] = "Search Keyword"
+            properties["OM.ossr"] = "Number of Search Results"
+            Visilabs.callAPI().customEvent("In App Search", properties: properties)
+            return
+        case .bannerClick:
+            properties["OM.OSB"] = "Banner Name/Banner Code"
+            Visilabs.callAPI().customEvent("Banner Click", properties: properties)
+            return
+        case .addToFavourites:
+            properties["OM.pf"] = "Product Code"
+            properties["OM.pfu"] = "1"
+            properties["OM.ppr"] = "Product Price"
+            Visilabs.callAPI().customEvent("Add To Favorites", properties: properties)
+            return
+        case .removeFromFavourites:
+            properties["OM.pf"] = "Product Code"
+            properties["OM.pfu"] = "-1"
+            properties["OM.ppr"] = "Product Price"
+            Visilabs.callAPI().customEvent("Add To Favorites", properties: properties)
+            return
+        case .sendingCampaignParameters:
+            properties["utm_source"] = "euromsg"
+            properties["utm_medium"] = "push"
+            properties["utm_campaign"] = "euromsg campaign"
+            Visilabs.callAPI().customEvent("Login Page", properties: properties)
+            return
+        case .pushMessage:
+            properties["OM.sys.TokenID"] = "Token ID to use for push messages"
+            properties["OM.sys.AppID"] = "App ID to use for push messages"
+            Visilabs.callAPI().customEvent("RegisterToken", properties: properties)
+            return
         }
+        
     }
     
 
