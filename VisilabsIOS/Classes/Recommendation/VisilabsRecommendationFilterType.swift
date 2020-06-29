f1fdba207727b060f70e3a50bb58e888a3fd0544
cicimen <egemengulkilik@gmail.com>
2020-06-29 17:27:53 +0300

    VisilabsProductAttribute enum add

--- VisilabsIOS/Classes/Recommendation/VisilabsRecommendationFilterType.swift
+++ VisilabsIOS/Classes/Recommendation/VisilabsRecommendationFilterType.swift
@@ -15,3 +15,24 @@ public class VisilabsRecommendationFilterType {
     static let greaterOrEquals = 6
     static let lessOrEquals = 7
 }
+
+public enum VisilabsProductAttribute: String {
+    case title
+    case img
+    case code
+    case target
+    case dest_url
+    case brand
+    case price
+    case dprice
+    case cur
+    case rating
+    case comment
+    case freeshipping
+    case samedayshipping
+    case attr1
+    case attr2
+    case attr3
+    case attr4
+    case attr5
+}
