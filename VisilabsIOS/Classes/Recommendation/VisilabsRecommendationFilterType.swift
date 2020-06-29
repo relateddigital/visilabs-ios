ee810fd8730d98cc40481b5f370bcbdae7a59738
cicimen <egemengulkilik@gmail.com>
2020-06-29 16:21:44 +0300

    VisilabsRecommendationFilterType implement

---
+++ VisilabsIOS/Classes/Recommendation/VisilabsRecommendationFilterType.swift
@@ -0,0 +1,17 @@
+//
+//  VisilabsRecommendationFilterType.swift
+//  VisilabsIOS
+//
+//  Created by Egemen on 29.06.2020.
+//
+
+public class VisilabsRecommendationFilterType {
+    static let equals = 0
+    static let notEquals = 1
+    static let like = 2, include = 2
+    static let notLike = 3, exclude = 3
+    static let greaterThan = 4
+    static let lessThan = 5
+    static let greaterOrEquals = 6
+    static let lessOrEquals = 7
+}
