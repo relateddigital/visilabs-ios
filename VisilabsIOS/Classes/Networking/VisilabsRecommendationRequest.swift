9eca18b9eeb712f66029795f18a2954954944d05
cicimen <egemengulkilik@gmail.com>
2020-06-30 18:55:17 +0300

    VisilabsRecommendationRequest implement

---
+++ VisilabsIOS/Classes/Networking/VisilabsRecommendationRequest.swift
@@ -0,0 +1,44 @@
+//
+//  VisilabsRecommendationRequest.swift
+//  VisilabsIOS
+//
+//  Created by Egemen on 30.06.2020.
+//
+
+import Foundation
+
+class VisilabsRecommendationRequest {
+    
+    //TODO: completion Any mi olmalı, yoksa AnyObject mi?
+    class func sendRequest(properties: [String : String], headers: [String : String], timeoutInterval: TimeInterval, completion: @escaping ([Any]?) -> Void) {
+        var queryItems = [URLQueryItem]()
+        for property in properties{
+            queryItems.append(URLQueryItem(name: property.key, value: property.value))
+        }
+        
+        let responseParser: (Data) -> [Any]? = { data in
+            var response: Any? = nil
+            do {
+                response = try JSONSerialization.jsonObject(with: data, options: [])
+            } catch {
+                VisilabsLogger.warn(message: "exception decoding api data")
+            }
+            return response as? [Any]
+        }
+        
+        let resource = VisilabsNetwork.buildResource(endPoint: .target, method: .get, timeoutInterval: timeoutInterval, requestBody: nil, queryItems: queryItems, headers: headers, parse: responseParser )
+        
+        sendRequestHandler(resource: resource, completion: { result in completion(result) })
+        
+    }
+    
+    private class func sendRequestHandler(resource: VisilabsResource<[Any]>, completion: @escaping ([Any]?) -> Void) {
+        VisilabsNetwork.apiRequest(resource: resource,
+            failure: { (reason, data, response) in
+                VisilabsLogger.warn(message: "API request to \(resource.endPoint) has failed with reason \(reason)")
+                completion(nil)
+            }, success: { (result, response) in
+                completion(result)
+            })
+    }
+}
