4a4baf44966135da108e1aa0488c81208e95f6a2
cicimen <egemengulkilik@gmail.com>
2020-07-01 16:39:28 +0300

    VisilabsRecommendationResponse update

--- VisilabsIOS/Classes/Recommendation/VisilabsRecommendationFilterType.swift
+++ VisilabsIOS/Classes/Recommendation/VisilabsRecommendationFilterType.swift
@@ -97,13 +97,11 @@ public class VisilabsProduct {
 
 public class VisilabsRecommendationResponse {
     var products: [VisilabsProduct]
-    var error: Error?
+    var error: VisilabsReason?
     var rawResponseString: String?
-    var responseStatusCode: Int
     
-    internal init(products: [VisilabsProduct], responseStatusCode: Int, rawResponseString: String? = nil, error: Error? = nil) {
+    internal init(products: [VisilabsProduct], rawResponseString: String? = nil, error: VisilabsReason? = nil) {
         self.products = products
-        self.responseStatusCode = responseStatusCode
         self.rawResponseString = rawResponseString
         self.error = error
     }
