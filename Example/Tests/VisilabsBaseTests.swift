dc7106746974158aff41d0ad04e22b1ea45a5d57
cicimen <egemengulkilik@gmail.com>
2020-07-07 17:41:26 +0300

    VisilabsBaseTests setup call createAPI

--- Example/Tests/VisilabsBaseTests.swift
+++ Example/Tests/VisilabsBaseTests.swift
@@ -7,7 +7,30 @@
 //
 
 import XCTest
+import VisilabsIOS
 
 class VisilabsBaseTests: XCTestCase {
+    
+    var visilabs: VisilabsInstance!
+    
+    override func setUp(){
+        print("Visilabs test setup starting")
+        super.setUp()
+        
+        //TODO: buraya stub'lar gelecek.
+        
+        
+        visilabs = Visilabs.createAPI(organizationId: "", siteId: "", loggerUrl: "", dataSource: "", realTimeUrl: "", channel: "", requestTimeoutInSeconds: 0, targetUrl: "", actionUrl: "", geofenceUrl: "", geofenceEnabled: false, maxGeofenceCount: 0, restUrl: "", encryptedDataSource: "")
+        
+        print("Visilabs test setup finished")
+        
+        
+    }
+    
+    override func tearDown() {
+        
+        super.tearDown()
+    }
+    
     
 }
