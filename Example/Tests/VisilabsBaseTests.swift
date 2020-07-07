4ee76da06dbe87a71191ddf2ba5efe9b0f91cb10
cicimen <egemengulkilik@gmail.com>
2020-07-07 14:23:35 +0300

    VisilabsBaseTests unit test class add

---
+++ Example/Tests/VisilabsBaseTests.swift
@@ -0,0 +1,13 @@
+//
+//  VisilabsBaseTests.swift
+//  VisilabsIOS_Tests
+//
+//  Created by Egemen on 7.07.2020.
+//  Copyright © 2020 CocoaPods. All rights reserved.
+//
+
+import XCTest
+
+class VisilabsBaseTests: XCTestCase {
+    
+}
