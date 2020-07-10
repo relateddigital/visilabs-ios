ce55e62054c3d3d3068ccb9544ea1a8b02162bbb
cicimen <egemengulkilik@gmail.com>
2020-07-10 17:52:17 +0300

    VisilabsGeofenceInstance locationManager add

--- VisilabsIOS/Classes/Geofence/VisilabsGeofenceInstance.swift
+++ VisilabsIOS/Classes/Geofence/VisilabsGeofenceInstance.swift
@@ -6,15 +6,74 @@
 //
 
 import Foundation
+import CoreLocation
 
-class VisilabsGeofenceInstance {
+
+
+public extension TimeInterval {
+    static var oneMinute: TimeInterval { return 60 }
+    static var oneHour: TimeInterval { return oneMinute * 60 }
+    static var oneDay: TimeInterval { return oneHour * 24 }
+    static var oneWeek: TimeInterval { return oneDay * 7 }
+    static var oneMonth: TimeInterval { return oneDay * 30 }
+    static var oneYear: TimeInterval { return oneDay * 365 }
+}
+
+class VisilabsGeofenceInstance : NSObject, CLLocationManagerDelegate {
     
+    internal var lastLocationManagerCreated: Date?
+    internal var maximumDesiredLocationAccuracy: CLLocationAccuracy = 30 // TODO: burada 30 yerine başka değer vermek doğru mu? önceden kCLLocationAccuracyHundredMeters kullanıyorduk.
     let organizationId: String
     let siteId: String
+    
+    lazy var locationManager: CLLocationManager = {
+        lastLocationManagerCreated = Date()
+
+        let manager = CLLocationManager()
+        manager.distanceFilter = kCLDistanceFilterNone
+        manager.desiredAccuracy = self.maximumDesiredLocationAccuracy
+        manager.pausesLocationUpdatesAutomatically = false
+        
+        manager.delegate = self
+
+        return manager
+    }()
+    
+    
 
     init(organizationId: String, siteId: String) {
         self.organizationId = organizationId
         self.siteId = siteId
+    }
+    
+    
+    // MARK: - iOS bug workaround
+
+    // to work around an iOS 13.3 bug that results in the location manager "dying", no longer receiving location updates
+    public func recreateTheLocationManager() {
+
+        // don't recreate location managers too often
+        //if let last = lastLocationManagerCreated, last.age! < .oneMinute { return }
+
+        if let llmc = lastLocationManagerCreated, llmc > Date().addingTimeInterval(-TimeInterval.oneMinute) {
+            return
+        }
+        
+        lastLocationManagerCreated = Date()
+
+        let freshManager = CLLocationManager()
+        freshManager.distanceFilter = locationManager.distanceFilter
+        freshManager.desiredAccuracy = locationManager.desiredAccuracy
+        freshManager.pausesLocationUpdatesAutomatically = false
+        freshManager.allowsBackgroundLocationUpdates = true
+        freshManager.delegate = self
+
+        // hand over to new manager
+        freshManager.startUpdatingLocation()
+        locationManager.stopUpdatingLocation()
+        locationManager = freshManager
+
+        VisilabsLogger.warn(message: "Recreated the LocationManager")
     }
     
 }
