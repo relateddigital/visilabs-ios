06128a406ba3f40c53fdb6ddbd35d2184d856693
cicimen <egemengulkilik@gmail.com>
2020-08-07 11:29:13 +0300

    all requests move to VisilabsRequest class

--- VisilabsIOS/Classes/Networking/VisilabsRequest.swift
+++ VisilabsIOS/Classes/Networking/VisilabsRequest.swift
@@ -7,8 +7,10 @@
 
 import Foundation
 
-class VisilabsRequest: VisilabsNetwork {
+class VisilabsRequest {
 
+    // MARK: - Event
+    
     class func sendEventRequest(visilabsEndpoint: VisilabsEndpoint, properties: [String : String], headers: [String : String], timeoutInterval: TimeInterval, completion: @escaping ([String : String]?) -> Void) {
 
         var queryItems = [URLQueryItem]()
@@ -59,5 +61,76 @@ class VisilabsRequest: VisilabsNetwork {
             }
         }
         return cookieKeyValues
+    }
+    
+    
+    // MARK: -Recommendation
+    
+    //TODO: completion Any mi olmalı, yoksa AnyObject mi?
+    class func sendRecommendationRequest(properties: [String : String], headers: [String : String], timeoutInterval: TimeInterval, completion: @escaping ([Any]?, VisilabsReason?) -> Void) {
+        var queryItems = [URLQueryItem]()
+        for property in properties{
+            queryItems.append(URLQueryItem(name: property.key, value: property.value))
+        }
+        
+        let responseParser: (Data) -> [Any]? = { data in
+            var response: Any? = nil
+            do {
+                response = try JSONSerialization.jsonObject(with: data, options: [])
+            } catch {
+                VisilabsLogger.warn(message: "exception decoding api data")
+            }
+            return response as? [Any]
+        }
+        
+        let resource = VisilabsNetwork.buildResource(endPoint: .target, method: .get, timeoutInterval: timeoutInterval, requestBody: nil, queryItems: queryItems, headers: headers, parse: responseParser )
+        
+        sendRecommendationRequestHandler(resource: resource, completion: { result, reason in completion(result, reason) })
+        
+    }
+    
+    private class func sendRecommendationRequestHandler(resource: VisilabsResource<[Any]>, completion: @escaping ([Any]?, VisilabsReason?) -> Void) {
+        VisilabsNetwork.apiRequest(resource: resource,
+            failure: { (reason, data, response) in
+                VisilabsLogger.warn(message: "API request to \(resource.endPoint) has failed with reason \(reason)")
+                completion(nil, reason)
+            }, success: { (result, response) in
+                completion(result, nil)
+            })
+    }
+    
+    // MARK: -InAppNotification
+    
+    //TODO: completion Any mi olmalı, yoksa AnyObject mi?
+    class func sendInAppNotificationRequest(properties: [String : String], headers: [String : String], timeoutInterval: TimeInterval, completion: @escaping ([[String: Any]]?) -> Void) {
+        var queryItems = [URLQueryItem]()
+        for property in properties{
+            queryItems.append(URLQueryItem(name: property.key, value: property.value))
+        }
+        
+        let responseParser: (Data) -> [[String: Any]]? = { data in
+            var response: Any? = nil
+            do {
+                response = try JSONSerialization.jsonObject(with: data, options: [])
+            } catch {
+                VisilabsLogger.warn(message: "exception decoding api data")
+            }
+            return response as? [[String: Any]]
+        }
+        
+        let resource = VisilabsNetwork.buildResource(endPoint: .action, method: .get, timeoutInterval: timeoutInterval, requestBody: nil, queryItems: queryItems, headers: headers, parse: responseParser )
+        
+        sendInAppNotificationRequestHandler(resource: resource, completion: { result in completion(result) })
+        
+    }
+    
+    private class func sendInAppNotificationRequestHandler(resource: VisilabsResource<[[String: Any]]>, completion: @escaping ([[String: Any]]?) -> Void) {
+        VisilabsNetwork.apiRequest(resource: resource,
+            failure: { (reason, data, response) in
+                VisilabsLogger.warn(message: "API request to \(resource.endPoint) has failed with reason \(reason)")
+                completion(nil)
+            }, success: { (result, response) in
+                completion(result)
+            })
     }
 }
