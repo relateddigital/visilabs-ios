f3c68777255a40cce779087b20a413297dcb4ef7
cicimen <egemengulkilik@gmail.com>
2020-08-24 17:00:06 +0300

    VisilabsReason conform to Codable protocol

--- VisilabsIOS/Classes/Networking/VisilabsNetwork.swift
+++ VisilabsIOS/Classes/Networking/VisilabsNetwork.swift
@@ -31,11 +31,56 @@ struct VisilabsResource<A> {
     let parse: (Data) -> A?
 }
 
-public enum VisilabsReason {
+public enum VisilabsReason: Codable {
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: Key.self)
+        let rawValue = try container.decode(Int.self, forKey: .rawValue)
+        switch rawValue {
+        case 0:
+            self = .parseError
+        case 1:
+            self = .noData
+        case 2:
+            let statusCode = try container.decode(Int.self, forKey: .associatedValue)
+            self = .notOKStatusCode(statusCode: statusCode)
+        case 3:
+            let errorDescription = try container.decode(String.self, forKey: .associatedValue)
+            self = .other(errorDescription: errorDescription)
+        default:
+            throw CodingError.unknownValue
+        }
+    }
+    
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: Key.self)
+        switch self {
+        case .parseError:
+             try container.encode(0, forKey: .rawValue)
+        case .noData:
+            try container.encode(1, forKey: .rawValue)
+        case .notOKStatusCode(let statusCode):
+            try container.encode(2, forKey: .rawValue)
+            try container.encode(statusCode, forKey: .associatedValue)
+        case .other(let errorDescription):
+            try container.encode(2, forKey: .rawValue)
+            try container.encode(errorDescription, forKey: .associatedValue)
+        }
+        
+    }
+    
+    enum Key: CodingKey {
+        case rawValue
+        case associatedValue
+    }
+    
+    enum CodingError: Error {
+        case unknownValue
+    }
+    
     case parseError
     case noData
     case notOKStatusCode(statusCode: Int)
-    case other(Error)
+    case other(errorDescription: String?)
 }
 
 struct VisilabsBasePath {
@@ -70,7 +115,7 @@ class VisilabsNetwork {
             guard let httpResponse = response as? HTTPURLResponse else {
 
                 if let hasError = error {
-                    failure(.other(hasError), data, response)
+                    failure(.other(errorDescription: hasError.localizedDescription), data, response)
                 } else {
                     failure(.noData, data, response)
                 }
