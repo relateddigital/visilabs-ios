4dcd32c59a98f86e65ea88a3aeb2734d699a9443
cicimen <egemengulkilik@gmail.com>
2020-08-24 14:24:38 +0300

    VisilabsConstants GEOFENCE_HISTORY_MAX_COUNT add

--- VisilabsIOS/Classes/Utilities/VisilabsConstants.swift
+++ VisilabsIOS/Classes/Utilities/VisilabsConstants.swift
@@ -10,6 +10,7 @@ import Foundation
 struct VisilabsConstants {
     
     static var QUEUE_SIZE = 5000
+    static let GEOFENCE_HISTORY_MAX_COUNT = 500 //TODO: bunu sonra değiştir
     
     //TODO: bunlar https e çevirilecek
     static var LOGGER_END_POINT = "http://lgr.visilabs.net"
