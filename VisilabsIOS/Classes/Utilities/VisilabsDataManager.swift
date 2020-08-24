6789958289a86afe68d0429d9379b813098b82b0
cicimen <egemengulkilik@gmail.com>
2020-08-24 13:25:01 +0300

    VisilabsDataManager visilabsProfile, visilabsUser and visilabsGeofenceHistory methods add

--- VisilabsIOS/Classes/Utilities/VisilabsDataManager.swift
+++ VisilabsIOS/Classes/Utilities/VisilabsDataManager.swift
@@ -9,6 +9,57 @@ import Foundation
 
 class VisilabsDataManager {
     
+    static func saveVisilabsProfile(_ visilabsProfile: VisilabsProfile) {
+        let encoder = JSONEncoder()
+        if let encodedVisilabsProfile = try? encoder.encode(visilabsProfile) {
+            save(VisilabsConstants.USER_DEFAULTS_PROFILE_KEY, withObject: encodedVisilabsProfile)
+        }
+    }
+    
+    static func readVisilabsProfile() -> VisilabsProfile? {
+        if let savedVisilabsProfile = read(VisilabsConstants.USER_DEFAULTS_PROFILE_KEY) as? Data {
+            let decoder = JSONDecoder()
+            if let loadedVisilabsProfile = try? decoder.decode(VisilabsProfile.self, from: savedVisilabsProfile) {
+                return loadedVisilabsProfile
+            }
+        }
+        return nil
+    }
+    
+    static func saveVisilabsUser(_ visilabsUser: VisilabsUser) {
+        let encoder = JSONEncoder()
+        if let encodedVisilabsUser = try? encoder.encode(visilabsUser) {
+            save(VisilabsConstants.USER_DEFAULTS_USER_KEY, withObject: encodedVisilabsUser)
+        }
+    }
+    
+    static func readVisilabsUser() -> VisilabsUser? {
+        if let savedVisilabsUser = read(VisilabsConstants.USER_DEFAULTS_USER_KEY) as? Data {
+            let decoder = JSONDecoder()
+            if let loadedVisilabsUser = try? decoder.decode(VisilabsUser.self, from: savedVisilabsUser) {
+                return loadedVisilabsUser
+            }
+        }
+        return nil
+    }
+    
+    static func saveVisilabsGeofenceHistory(_ visilabsGeofenceHistory: VisilabsGeofenceHistory) {
+        let encoder = JSONEncoder()
+        if let encodedVisilabsGeofenceHistory = try? encoder.encode(visilabsGeofenceHistory) {
+            save(VisilabsConstants.USER_DEFAULTS_GEOFENCE_HISTORY_KEY, withObject: encodedVisilabsGeofenceHistory)
+        }
+    }
+    
+    static func readVisilabsGeofenceHistory() -> VisilabsGeofenceHistory? {
+        if let savedVisilabsGeofenceHistory = read(VisilabsConstants.USER_DEFAULTS_GEOFENCE_HISTORY_KEY) as? Data {
+            let decoder = JSONDecoder()
+            if let loadedVisilabsGeofenceHistory = try? decoder.decode(VisilabsGeofenceHistory.self, from: savedVisilabsGeofenceHistory) {
+                return loadedVisilabsGeofenceHistory
+            }
+        }
+        return nil
+    }
+    
     static func save(_ key: String, withObject value: Any?) {
         UserDefaults.standard.set(value, forKey: key)
         UserDefaults.standard.synchronize()
