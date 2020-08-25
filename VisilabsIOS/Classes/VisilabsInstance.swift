f17202bf559189324a92545ef3479d63c650179e
cicimen <egemengulkilik@gmail.com>
2020-08-25 17:33:43 +0300

    VisilabsFavoriteAttribute enum move to new file in favoriteattributeaction folder

--- VisilabsIOS/Classes/VisilabsInstance.swift
+++ VisilabsIOS/Classes/VisilabsInstance.swift
@@ -291,27 +291,6 @@ extension VisilabsInstance {
     }
 }
 
-
-public enum VisilabsFavoriteAttribute: String {
-    case ageGroup
-    case attr1
-    case attr2
-    case attr3
-    case attr4
-    case attr5
-    case attr6
-    case attr7
-    case attr8
-    case attr9
-    case attr10
-    case brand
-    case category
-    case color
-    case gender
-    case material
-    case title
-}
-
 public class VisilabsFavoritesResponse {
     public var favorites: [VisilabsFavoriteAttribute: [String]]
     public var error: VisilabsReason?
