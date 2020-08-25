b9f0fca50c3e0b26d391725d2b9a9820f928b235
cicimen <egemengulkilik@gmail.com>
2020-08-25 19:28:17 +0300

    VisilabsStoryExtendedProperties and VisilabsStoryResponse classes add

---
+++ VisilabsIOS/Classes/TargetingAction/Story/VisilabsStoryExtendedProperties.swift
@@ -0,0 +1,10 @@
+//
+//  VisilabsStoryExtendedProperties.swift
+//  VisilabsIOS
+//
+//  Created by Egemen on 25.08.2020.
+//
+
+class VisilabsStoryExtendedProperties {
+    
+}
---
+++ VisilabsIOS/Classes/TargetingAction/Story/VisilabsStoryResponse.swift
@@ -0,0 +1,10 @@
+//
+//  VisilabsStoryResponse.swift
+//  VisilabsIOS
+//
+//  Created by Egemen on 25.08.2020.
+//
+
+class VisilabsStoryResponse {
+    
+}
