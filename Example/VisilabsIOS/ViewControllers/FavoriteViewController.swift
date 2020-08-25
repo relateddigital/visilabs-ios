1c2ba565680f7cd4889ad873b94df9b40c85b917
cicimen <egemengulkilik@gmail.com>
2020-08-25 14:15:47 +0300

    example project FavoriteViewController class add

---
+++ Example/VisilabsIOS/ViewControllers/FavoriteViewController.swift
@@ -0,0 +1,13 @@
+//
+//  FavoriteViewController.swift
+//  VisilabsIOS_Example
+//
+//  Created by Egemen on 25.08.2020.
+//  Copyright © 2020 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class FavoriteViewController: UIViewController {
+    
+}
