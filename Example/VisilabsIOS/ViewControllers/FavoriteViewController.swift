1dcd92e95f36bfb03499fc2bfc93788ea31cb139
cicimen <egemengulkilik@gmail.com>
2020-08-25 15:04:06 +0300

    example project FavoriteViewController first form section add

--- Example/VisilabsIOS/ViewControllers/FavoriteViewController.swift
+++ Example/VisilabsIOS/ViewControllers/FavoriteViewController.swift
@@ -6,8 +6,23 @@
 //  Copyright © 2020 CocoaPods. All rights reserved.
 //
 
+import Foundation
+import Eureka
 import UIKit
+import VisilabsIOS
 
-class FavoriteViewController: UIViewController {
+class FavoriteViewController: FormViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        initializeForm()
+    }
     
+    private func initializeForm() {
+        form +++
+            
+        Section("Favorite Attribute Actions".uppercased(with: Locale(identifier: "en_US")))
+        
+    }
+
 }
