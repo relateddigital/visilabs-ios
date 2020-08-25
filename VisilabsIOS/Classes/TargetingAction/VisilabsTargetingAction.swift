3b4deed25a2b9f36aa02e5eadfde73984c61fc6e
cicimen <egemengulkilik@gmail.com>
2020-08-25 17:29:00 +0300

    VisilabsTargetingAction visilabsProfile add to constructor and start to implement parseFavoritesResponse method

--- VisilabsIOS/Classes/TargetingAction/VisilabsTargetingAction.swift
+++ VisilabsIOS/Classes/TargetingAction/VisilabsTargetingAction.swift
@@ -8,6 +8,17 @@
 import Foundation
 
 class VisilabsTargetingAction {
+    
+    
+    let visilabsProfile: VisilabsProfile
+    
+    required init(lock: VisilabsReadWriteLock, visilabsProfile: VisilabsProfile) {
+        self.notificationsInstance = VisilabsInAppNotifications(lock: lock)
+        self.visilabsProfile = visilabsProfile
+    }
+    
+    // MARK: - InApp Notifications
+    
     var notificationsInstance: VisilabsInAppNotifications
     
     var inAppDelegate: VisilabsInAppNotificationsDelegate? {
@@ -19,12 +30,7 @@ class VisilabsTargetingAction {
         }
     }
     
-    let visilabsProfile: VisilabsProfile
     
-    required init(lock: VisilabsReadWriteLock, visilabsProfile: VisilabsProfile) {
-        self.notificationsInstance = VisilabsInAppNotifications(lock: lock)
-        self.visilabsProfile = visilabsProfile
-    }
     
     func checkInAppNotification(properties: [String:String], visilabsUser: VisilabsUser, timeoutInterval: TimeInterval, completion: @escaping ((_ response: VisilabsInAppNotification?) -> Void)){
         let semaphore = DispatchSemaphore(value: 0)
@@ -63,8 +69,55 @@ class VisilabsTargetingAction {
     
     // MARK: - Favorites
     
-    func getFavorites(actionId: Int? = nil, completion: @escaping ((_ response: VisilabsFavoritesResponse) -> Void)){
+    //class func sendMobileRequest(properties: [String : String], headers: [String : String], timeoutInterval: TimeInterval, completion: @escaping ([String: Any]?) -> Void) {
+    //https://s.visilabs.net/mobile?OM.oid=676D325830564761676D453D&OM.siteID=356467332F6533766975593D&OM.cookieID=B220EC66-A746-4130-93FD-53543055E406&OM.exVisitorID=ogun.ozturk%40euromsg.com&action_id=188&action_type=FavoriteAttributeAction&OM.apiver=IOS
+    func getFavorites(visilabsUser: VisilabsUser, actionId: Int? = nil, completion: @escaping ((_ response: VisilabsFavoritesResponse) -> Void)){
         
+        var props = [String: String]()
+        props[VisilabsConstants.ORGANIZATIONID_KEY] = self.visilabsProfile.organizationId
+        props[VisilabsConstants.PROFILEID_KEY] = self.visilabsProfile.profileId
+        props[VisilabsConstants.COOKIEID_KEY] = visilabsUser.cookieId
+        props[VisilabsConstants.EXVISITORID_KEY] = visilabsUser.exVisitorId
+        props[VisilabsConstants.TOKENID_KEY] = visilabsUser.tokenId
+        props[VisilabsConstants.APPID_KEY] = visilabsUser.appId
+        props[VisilabsConstants.APIVER_KEY] = VisilabsConstants.APIVER_VALUE
+        props[VisilabsConstants.ACTION_TYPE] = VisilabsConstants.FAVORITE_ATTRIBUTE_ACTION
+        props[VisilabsConstants.ACTION_ID] = actionId == nil ? nil : String(actionId!)
+        
+        VisilabsRequest.sendMobileRequest(properties: props, headers: [String : String](), timeoutInterval: self.visilabsProfile.requestTimeoutInterval, completion: { (result:[String: Any]?, reason: VisilabsReason?) in
+            
+
+            if reason != nil {
+                //completion(VisilabsRecommendationResponse(products: [VisilabsProduct](), error: reason))
+            }else {
+                for r in result!{
+                    if let product = VisilabsProduct(JSONObject: r as? [String: Any?]) {
+                        products.append(product)
+                    }
+                }
+                
+                completion(VisilabsRecommendationResponse(products: products, error: nil))
+            }
+        })
+        
+    }
+    
+    //{"capping":"{\"data\":{}}","VERSION":1,"FavoriteAttributeAction":[{"actid":188,"title":"fav-test","actiontype":"FavoriteAttributeAction","actiondata":{"attributes":["category","brand"],"favorites":{"category":["6","8","2"],"brand":["Kozmo","Luxury Room","OFS"]}}}]}
+    private func parseFavoritesResponse(result:[String: Any]?, reason: VisilabsReason?) -> VisilabsFavoritesResponse {
+        if let error = reason {
+            return VisilabsFavoritesResponse(favorites: [VisilabsFavoriteAttribute : [String]](), error: error)
+        } else if let res = result {
+            if let favoriteAttributeActions = res[VisilabsConstants.FAVORITE_ATTRIBUTE_ACTION] as? [[String: Any?]] {
+                for favoriteAttributeAction in favoriteAttributeActions {
+                    if let actiondata = favoriteAttributeAction[VisilabsConstants.ACTIONDATA] as? [String: Any?] {
+                        if let favorites = actiondata[VisilabsConstants.FAVORITES] as? [String: [String]?]{
+                            
+                        }
+                    }
+                }
+            }
+        }
+        return VisilabsFavoritesResponse(favorites: [VisilabsFavoriteAttribute : [String]](), error: VisilabsReason.noData)
     }
     
 }
