594d5021f632d2b1ecb7640073b3e38dc0ea0b26
cicimen <egemengulkilik@gmail.com>
2020-08-31 16:49:33 +0300

    VisilabsStoryResponse props and init add

--- VisilabsIOS/Classes/TargetingAction/Story/VisilabsStoryResponse.swift
+++ VisilabsIOS/Classes/TargetingAction/Story/VisilabsStoryResponse.swift
@@ -5,6 +5,20 @@
 //  Created by Egemen on 25.08.2020.
 //
 
+enum VisilabsStoryTemplate : String {
+    case StoryLookingBanners
+}
+
 class VisilabsStoryResponse {
+    public var stories: [VisilabsStory]
+    public var error: VisilabsReason?
+    public var storyTemplate: VisilabsStoryTemplate
+    public var storyExtendedProperties: VisilabsStoryExtendedProperties
     
+    internal init(storyTemplate: VisilabsStoryTemplate, stories: [VisilabsStory], storyExtendedProperties: VisilabsStoryExtendedProperties, error: VisilabsReason? = nil) {
+        self.storyTemplate = storyTemplate
+        self.stories = stories
+        self.storyExtendedProperties = storyExtendedProperties
+        self.error = error
+    }
 }
