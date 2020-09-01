2cb7e61289ebb3b68656d85447852043c2a5f664
cicimen <egemengulkilik@gmail.com>
2020-09-01 18:34:53 +0300

    example project ModalViewController comments delete

--- Example/VisilabsIOS/ViewControllers/ModalViewController.swift
+++ Example/VisilabsIOS/ViewControllers/ModalViewController.swift
@@ -29,35 +29,16 @@ class ModalViewController: UIViewController {
 
         popupView.clipsToBounds = true
         popupView.layer.cornerRadius = 6
-
-        /*
-        let tap = UITapGestureRecognizer(target: self, action: #selector(handleTap))
-        view.addGestureRecognizer(tap)
-*/
-    
         super.viewDidLoad()
         colorPickerView.addTarget(self, action: #selector(self.handleColorChanged(picker:)), for: .valueChanged)
         colorPickerView.set(color: selectedColor, colorSpace: colorSpace)
         headerLabel.text = headerText
-        //updateColorSpaceText()
-        //handleColorChanged(picker: popupView)
     }
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        
-        /*
-        DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
-            self.dismiss(animated: true, completion: nil)
-        }
- */
     }
 
-    /*
-    @objc func handleTap(gesture: UITapGestureRecognizer) {
-        dismiss(animated: true, completion: nil)
-    }
- */
     
     @IBAction func handleSelect(_ sender: Any) {
         self.textRow.value = colorPickerView.color.toHexString()
@@ -67,43 +48,6 @@ class ModalViewController: UIViewController {
     
     @objc func handleColorChanged(picker: ColorPicker) {
         let color = picker.color
-        //label.text = picker.color.description
     }
-    
-    /*
-    
-
-    @IBAction func handleRedButtonAction(_ sender: UIButton) {
-        colorPicker.set(color: .red, colorSpace: colorSpace)
-        handleColorChanged(picker: colorPicker)
-    }
-
-    @IBAction func handlePurpleButtonAction(_ sender: UIButton) {
-        colorPicker.set(color: .purple, colorSpace: colorSpace)
-        handleColorChanged(picker: colorPicker)
-    }
-
-    @IBAction func handleYellowButtonAction(_ sender: UIButton) {
-        colorPicker.set(color: .yellow, colorSpace: colorSpace)
-        handleColorChanged(picker: colorPicker)
-    }
-
-    @IBAction func handleSwitchAction(_ sender: UISwitch) {
-        colorSpace = sender.isOn ? .extendedSRGB : .sRGB
-        colorPicker.set(color: colorPicker.color, colorSpace: colorSpace)
-        updateColorSpaceText()
-        handleColorChanged(picker: colorPicker)
-    }
-
-    func updateColorSpaceText() {
-        switch colorSpace {
-        case .extendedSRGB:
-            colorSpaceLabel.text = "Extended sRGB"
-        case .sRGB:
-            colorSpaceLabel.text = "sRGB"
-        }
-    }
-     */
-
 }
 
