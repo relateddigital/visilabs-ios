2c2c9006fea8445230a145df5bfd653c9483bad2
cicimen <egemengulkilik@gmail.com>
2020-10-08 19:55:40 +0300

    VisilabsStoryPreviewView view add

---
+++ VisilabsIOS/Classes/TargetingAction/Story/Views/VisilabsStoryPreviewView.swift
@@ -0,0 +1,74 @@
+//
+//  VisilabsStoryPreviewView.swift
+//  VisilabsIOS
+//
+//  Created by Egemen on 7.10.2020.
+//
+
+import UIKit
+
+public enum VisilabsLayoutType {
+    case cubic
+    var animator: LayoutAttributesAnimator {
+        switch self {
+        case .cubic:return CubeAttributesAnimator(perspective: -1/100, totalAngle: .pi/12)
+        }
+    }
+}
+
+class VisilabsStoryPreviewView: UIView {
+    
+    //MARK:- iVars
+    var layoutType: VisilabsLayoutType?
+    /**Layout Animate options(ie.choose which kinda animation you want!)*/
+    lazy var layoutAnimator: (LayoutAttributesAnimator, Bool, Int, Int) = (layoutType!.animator, true, 1, 1)
+    lazy var snapsCollectionViewFlowLayout: AnimatedCollectionViewLayout = {
+        let flowLayout = AnimatedCollectionViewLayout()
+        flowLayout.scrollDirection = .horizontal
+        flowLayout.animator = layoutAnimator.0
+        flowLayout.minimumLineSpacing = 0.0
+        flowLayout.minimumInteritemSpacing = 0.0
+        flowLayout.sectionInset = UIEdgeInsets(top: 0.0, left: 0.0, bottom: 0.0, right: 0.0)
+        return flowLayout
+    }()
+    lazy var snapsCollectionView: UICollectionView! = {
+        let cv = UICollectionView.init(frame: CGRect(x: 0,y: 0,width: UIScreen.main.bounds.width,height:  UIScreen.main.bounds.height), collectionViewLayout: snapsCollectionViewFlowLayout)
+        cv.backgroundColor = .black
+        cv.showsVerticalScrollIndicator = false
+        cv.showsHorizontalScrollIndicator = false
+        cv.register(VisilabsStoryPreviewCell.self, forCellWithReuseIdentifier: VisilabsStoryPreviewCell.reuseIdentifier)
+        cv.translatesAutoresizingMaskIntoConstraints = false
+        cv.isPagingEnabled = true
+        cv.isPrefetchingEnabled = false
+        cv.collectionViewLayout = snapsCollectionViewFlowLayout
+        return cv
+    }()
+    
+    //MARK:- Overridden functions
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    convenience init(layoutType: VisilabsLayoutType) {
+        self.init()
+        self.layoutType = layoutType
+        createUIElements()
+        installLayoutConstraints()
+    }
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    //MARK: - Private functions
+    private func createUIElements(){
+        backgroundColor = .black
+        addSubview(snapsCollectionView)
+    }
+    private func installLayoutConstraints(){
+        //Setting constraints for snapsCollectionview
+        NSLayoutConstraint.activate([
+            igLeftAnchor.constraint(equalTo: snapsCollectionView.igLeftAnchor),
+            igTopAnchor.constraint(equalTo: snapsCollectionView.igTopAnchor),
+            snapsCollectionView.igRightAnchor.constraint(equalTo: igRightAnchor),
+            snapsCollectionView.igBottomAnchor.constraint(equalTo: igBottomAnchor)])
+    }
+}
