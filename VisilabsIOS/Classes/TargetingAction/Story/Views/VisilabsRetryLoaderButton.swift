5aa358b0f4e77377e9cf031f1ceae3c3b4164cad
cicimen <egemengulkilik@gmail.com>
2020-10-08 19:14:52 +0300

    VisilabsRetryLoaderButton view add

---
+++ VisilabsIOS/Classes/TargetingAction/Story/Views/VisilabsRetryLoaderButton.swift
@@ -0,0 +1,39 @@
+//
+//  VisilabsRetryLoaderButton.swift
+//  VisilabsIOS
+//
+//  Created by Egemen on 8.10.2020.
+//
+
+import Foundation
+import UIKit
+
+protocol RetryBtnDelegate: class {
+    func retryButtonTapped()
+}
+
+public class VisilabsRetryLoaderButton: UIButton {
+    var contentURL: String?
+    weak var delegate: RetryBtnDelegate?
+    convenience init(withURL url: String) {
+        self.init()
+        self.backgroundColor = .white
+        self.frame = CGRect(x: 0, y: 0, width: 60, height: 60)
+        self.setImage(#imageLiteral(resourceName: "ic_retry"), for: .normal)
+        self.addTarget(self, action: #selector(didTapRetryBtn), for: .touchUpInside)
+        self.contentURL = url
+        self.tag = 100
+    }
+    @objc func didTapRetryBtn() {
+        delegate?.retryButtonTapped()
+    }
+}
+
+extension UIView {
+    func removeRetryButton() {
+        self.subviews.forEach({v in
+            if(v.tag == 100){v.removeFromSuperview()}
+        })
+    }
+    
+}
