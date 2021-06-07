3cdc093c4304157d2138981305d64e2a3547ee78
cicimen <egemengulkilik@gmail.com>
2021-06-08 00:15:56 +0300

    Package.swift file add

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "VisilabsIOS",
+    platforms: [.iOS(.v10)],
+    products: [
+        .library(
+            name: "VisilabsIOS",
+            targets: ["VisilabsIOS"])
+    ],
+    targets: [
+        .target(
+            name: "VisilabsIOS",
+            path: "Sources",
+	   resources: .process("Assets"))
+    ]
+)
