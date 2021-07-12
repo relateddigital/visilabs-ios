4adc9d99278fce7ff932e811a5b05c88a7ec4b91
cicimen <egemengulkilik@gmail.com>
2021-07-12 17:14:08 +0300

    version upgrade to 3.0.6

--- Sources/VisilabsInstance.swift
+++ Sources/VisilabsInstance.swift
@@ -162,7 +162,7 @@ public class VisilabsInstance: CustomDebugStringConvertible {
             }
         }
 
-        visilabsUser.sdkVersion = "3.0.4"
+        visilabsUser.sdkVersion = "3.0.6"
         #if SWIFT_PACKAGE
             let bundle = Bundle.module
         #else
