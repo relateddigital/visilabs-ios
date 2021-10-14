421a12386390e0687e1b22078cd0f608caf09053
cicimen <egemengulkilik@gmail.com>
2021-10-14 14:35:26 +0300

    VisilabsStoryHomeViewController fix for UINavigationController and UITabBarController

--- Sources/TargetingAction/Story/Controllers/VisilabsStoryHomeViewController.swift
+++ Sources/TargetingAction/Story/Controllers/VisilabsStoryHomeViewController.swift
@@ -12,44 +12,44 @@ public class VisilabsStoryHomeViewController: NSObject,
                                               UICollectionViewDataSource,
                                               UICollectionViewDelegate,
                                               UICollectionViewDelegateFlowLayout {
-
+    
     weak var collectionView: UICollectionView! {
         didSet {
             collectionView.dataSource = self
             collectionView.delegate = self
         }
     }
-
+    
     public weak var urlDelegate: VisilabsStoryURLDelegate?
-
+    
     var storyAction: VisilabsStoryAction!
     var storiesLoaded = false
-
+    
     func loadStoryAction(_ storyAction: VisilabsStoryAction) {
         self.storyAction = storyAction
         self.storiesLoaded = true
     }
-
+    
     public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return storiesLoaded ? storyAction.stories.count : 1
     }
-
+    
     public func collectionView(_ collectionView: UICollectionView,
                                cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         if !storiesLoaded {
             guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier:
-                      VisilabsStoryHomeViewCell.reuseIdentifier, for: indexPath)
+                                                                    VisilabsStoryHomeViewCell.reuseIdentifier, for: indexPath)
                     as? VisilabsStoryHomeViewCell else {
-                return UICollectionViewCell()
-            }
+                        return UICollectionViewCell()
+                    }
             cell.setAsLoadingCell()
             return cell
         } else {
             guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier:
-                    VisilabsStoryHomeViewCell.reuseIdentifier, for: indexPath)
+                                                                    VisilabsStoryHomeViewCell.reuseIdentifier, for: indexPath)
                     as? VisilabsStoryHomeViewCell else {
-                return UICollectionViewCell()
-            }
+                        return UICollectionViewCell()
+                    }
             if self.storyAction.extendedProperties.moveShownToEnd {
                 self.storyAction.stories = sortStories(stories: self.storyAction.stories)
             }
@@ -59,7 +59,7 @@ public class VisilabsStoryHomeViewController: NSObject,
             return cell
         }
     }
-
+    
     public func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         if self.storyAction.stories.count == 0 {
             return
@@ -67,7 +67,7 @@ public class VisilabsStoryHomeViewController: NSObject,
         let title = self.storyAction.stories[indexPath.row].title ?? ""
         let actid = self.storyAction.actionId
         self.setStoryShown(title: title, actid: actid)
-
+        
         if self.storyAction.storyTemplate == .skinBased {
             DispatchQueue.main.async {
                 for index in self.storyAction.stories.indices {
@@ -76,15 +76,13 @@ public class VisilabsStoryHomeViewController: NSObject,
                     self.storyAction.stories[index].isCancelledAbruptly = false
                 }
                 let storyPreviewScene = VisilabsStoryPreviewController.init(stories: self.storyAction.stories,
-                                                handPickedStoryIndex: indexPath.row, handPickedSnapIndex: 0)
+                                                                            handPickedStoryIndex: indexPath.row, handPickedSnapIndex: 0)
                 storyPreviewScene.storyUrlDelegate = self.urlDelegate
                 storyPreviewScene.modalPresentationStyle = .fullScreen
-                let app = VisilabsInstance.sharedUIApplication()
-                app?.keyWindow?.rootViewController?.present(storyPreviewScene, animated: true, completion: collectionView.reloadData)
-                // TO_DO: burada keywindow rootViewController yaklaşımı uygun mu?
+                self.getRootViewController()?.present(storyPreviewScene, animated: true, completion: collectionView.reloadData)
             }
         } else {
-
+            
             if self.storyAction.clickQueryItems.count > 0 {
                 Visilabs.callAPI().customEvent(VisilabsConstants.omEvtGif, properties: self.storyAction.clickQueryItems)
             }
@@ -98,16 +96,16 @@ public class VisilabsStoryHomeViewController: NSObject,
             }
         }
     }
-
+    
     public func collectionView(_ collectionView: UICollectionView,
                                layout collectionViewLayout: UICollectionViewLayout,
                                sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize(width: 80, height: 100)
     }
-
+    
     // First not shown stories
     private func sortStories(stories: [VisilabsStory]) -> [VisilabsStory] {
-
+        
         var shownStories: [VisilabsStory] = []
         var notShownStories: [VisilabsStory] = []
         for story in stories {
@@ -126,11 +124,11 @@ public class VisilabsStoryHomeViewController: NSObject,
         }
         return notShownStories + shownStories
     }
-
+    
     private func setStoryShown(title: String, actid: Int) {
         // Save UserDefaults as shown
         var shownStories = UserDefaults.standard.dictionary(forKey: VisilabsConstants.shownStories)
-            as? [String: [String]] ?? [String: [String]]()
+        as? [String: [String]] ?? [String: [String]]()
         if shownStories["\(actid)"] == nil {
             shownStories["\(actid)"] = [title]
         } else if let st = shownStories["\(actid)"], !st.contains(title) {
@@ -138,6 +136,31 @@ public class VisilabsStoryHomeViewController: NSObject,
         }
         UserDefaults.standard.setValue(shownStories, forKey: VisilabsConstants.shownStories)
     }
+    
+    func getRootViewController() -> UIViewController? {
+        guard let sharedUIApplication = VisilabsInstance.sharedUIApplication() else {
+            return nil
+        }
+        if let rootViewController = sharedUIApplication.keyWindow?.rootViewController {
+            return getVisibleViewController(rootViewController)
+        }
+        return nil
+    }
+    
+    private func getVisibleViewController(_ vc: UIViewController?) -> UIViewController? {
+        if vc is UINavigationController {
+            return getVisibleViewController((vc as? UINavigationController)?.visibleViewController)
+        } else if vc is UITabBarController {
+            return getVisibleViewController((vc as? UITabBarController)?.selectedViewController)
+        } else {
+            if let pvc = vc?.presentedViewController {
+                return getVisibleViewController(pvc.presentedViewController)
+            } else {
+                return vc
+            }
+        }
+    }
+    
 }
 
 @objc
