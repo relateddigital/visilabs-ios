a572b28e2ea69e30c0e9bc6b522e0521b15e85fa
orhun <akmilorhun@gmail.com>
2022-01-21 19:30:17 +0300

    sideBarController düzenlemeleri

--- Sources/TargetingAction/visilabsSideBarViewController.swift
+++ Sources/TargetingAction/visilabsSideBarViewController.swift
@@ -6,61 +6,31 @@
 //
 
 import Foundation
-
 import UIKit
-
-
-class SideBarModel {
-    
-    var text : String?
-}
 
 
 class visilabsSideBarViewController : UIViewController {
     
     
     var position: CGPoint?
-    var model:SideBarModel?
-    var sideBarHeight = 175.0
-    var miniSideBarWidth = 40.0
+    var model = SideBarModel()
     var window: UIWindow?
     var globSidebarView : sideBarView?
-    var isCircle:Bool = true
     var sideBarOpen:Bool = false
     var sideBarFirstPosition:CGPoint?
     
     
-    //dummy
-    var titleStr = "denemekkkkkkkkkkkk"
-
-
-    
     public init(model:SideBarModel?) {
         super.init(nibName: nil, bundle: nil)
-        self.model = model
+        self.model = createDummyModel()
         let sidebarView : sideBarView = UIView.fromNib()
         globSidebarView = sidebarView
         addTapGestureToSideBarMiniView()
         addTapGestureToImageOfGranSideBar()
-        //is circle servisten gelen veriye göre beslenmeli
-        if isCircle {
+        if self.model.isCircle {
             self.globSidebarView!.isHidden = true
         }
         self.view = sidebarView
-    }
-    
-    
-    func addTapGestureToSideBarMiniView() {
-        let tap = UITapGestureRecognizer(target: self, action: #selector(self.viewClicked(_:)))
-        globSidebarView?.sideBarMiniView.addGestureRecognizer(tap)
-    }
-    
-    
-    func addTapGestureToImageOfGranSideBar() {
-        globSidebarView?.sideBarGrandContentImageView.isUserInteractionEnabled = true
-        let tap = UITapGestureRecognizer(target: self, action: #selector(self.imageClicked(_:)))
-        globSidebarView?.sideBarGrandContentImageView.addGestureRecognizer(tap)
-        
     }
     
     required init?(coder: NSCoder) {
@@ -74,52 +44,105 @@ class visilabsSideBarViewController : UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         sideBarFirstPosition = self.window?.layer.position
-        if isCircle {
+        if model.isCircle {
             configureCircleSideBar()
         }
     }
     
+    func createDummyModel()  -> SideBarModel {
+        let model = SideBarModel()
+        
+        model.titleString = "deneme"
+        model.isCircle = true
+        model.screenYcoordinate = .middle
+        model.screenXcoordinate = .left
+        model.labelType = .upToDown
+
+        return model
+    }
+        
     func configureCircleSideBar() {
-        let semiCirleLayer: CAShapeLayer = CAShapeLayer()
-        let arcCenter = CGPoint(x: globSidebarView!.sideBarMiniView.bounds.maxX, y: self.globSidebarView!.sideBarMiniView.bounds.maxY/2)
-        let circleRadius = self.globSidebarView!.sideBarMiniView.height / 2
-        let circlePath = UIBezierPath(arcCenter: arcCenter, radius: circleRadius, startAngle: CGFloat.pi/2, endAngle: CGFloat.pi*3/2 , clockwise: true)
-        semiCirleLayer.path = circlePath.cgPath
-        semiCirleLayer.fillColor = UIColor.red.cgColor
-        semiCirleLayer.zPosition = -1
-        self.globSidebarView!.sideBarMiniView.layer.addSublayer(semiCirleLayer)
-        self.globSidebarView!.sideBarMiniView.backgroundColor = .clear
-        self.globSidebarView!.isHidden = false
+
+        if model.screenXcoordinate == .right {
+            let semiCirleLayer: CAShapeLayer = CAShapeLayer()
+            let arcCenter = CGPoint(x: globSidebarView!.LeftSideBarMiniView.bounds.maxX, y: self.globSidebarView!.LeftSideBarMiniView.bounds.maxY/2)
+            let circleRadius = self.globSidebarView!.LeftSideBarMiniView.height / 2
+            let circlePath = UIBezierPath(arcCenter: arcCenter, radius: circleRadius, startAngle: CGFloat.pi/2, endAngle: CGFloat.pi*3/2 , clockwise: true)
+            semiCirleLayer.path = circlePath.cgPath
+            semiCirleLayer.fillColor = UIColor.red.cgColor
+            semiCirleLayer.zPosition = -1
+            self.globSidebarView!.LeftSideBarMiniView.layer.addSublayer(semiCirleLayer)
+            self.globSidebarView!.LeftSideBarMiniView.backgroundColor = .clear
+            self.globSidebarView!.isHidden = false
+        } else if model.screenXcoordinate == .left {
+            let semiCirleLayer: CAShapeLayer = CAShapeLayer()
+            let arcCenter = CGPoint(x: globSidebarView!.rightSideBarMiniView.bounds.minX, y: self.globSidebarView!.rightSideBarMiniView.bounds.maxY/2)
+            let circleRadius = self.globSidebarView!.rightSideBarMiniView.height / 2
+            let circlePath = UIBezierPath(arcCenter: arcCenter, radius: circleRadius, startAngle: CGFloat.pi/2, endAngle: CGFloat.pi*3/2 , clockwise: false)
+            semiCirleLayer.path = circlePath.cgPath
+            semiCirleLayer.fillColor = UIColor.red.cgColor
+            semiCirleLayer.zPosition = -1
+            self.globSidebarView!.rightSideBarMiniView.layer.addSublayer(semiCirleLayer)
+            self.globSidebarView!.rightSideBarMiniView.backgroundColor = .clear
+            self.globSidebarView!.isHidden = false
+            
+        }
+
         
     }
     
     func configureView(sideBar:sideBarView) {
-                
-        sideBar.titleLabel.transform = CGAffineTransform(rotationAngle: -CGFloat.pi / 2)
-        if titleStr.count > 10 {
-            while titleStr.count != 10 {
-                titleStr.removeLast()
-            }
+        
+        //ekranın sağında mı solunda mı
+        if self.model.screenXcoordinate == .right {
+            globSidebarView?.rightSideBarMiniWidthConstraint.constant = 0
+            globSidebarView?.rightSideBarMiniView.isHidden = true
+        } else if self.model.screenXcoordinate == .left {
+            globSidebarView?.LeftSideBarMiniWidthConstraint.constant = 0
+            globSidebarView?.LeftSideBarMiniView.isHidden = true
         }
-        sideBar.titleLabel.text = titleStr + " ^"
         
+        //label tipi
+        if self.model.labelType == .downToUp  && self.model.screenXcoordinate == .right {
+            globSidebarView!.LeftTitleLabel.transform = CGAffineTransform(rotationAngle: -CGFloat.pi / 2)
+        } else if self.model.labelType == .upToDown  && self.model.screenXcoordinate == .right {
+            globSidebarView!.LeftTitleLabel.transform = CGAffineTransform(rotationAngle: CGFloat.pi / 2)
+        } else if self.model.labelType == .downToUp  && self.model.screenXcoordinate == .left {
+            globSidebarView!.rightTitleLabel.transform = CGAffineTransform(rotationAngle: -CGFloat.pi / 2)
+        } else if self.model.labelType == .upToDown  && self.model.screenXcoordinate == .left {
+            globSidebarView!.rightTitleLabel.transform = CGAffineTransform(rotationAngle: CGFloat.pi / 2)
+        }
         
-        
-
-
-//        sideBar.labelSuperView.backgroundColor = .clear
-//        sideBar.titleLabel.font = sideBar.titleLabel.font.withSize(30)
-//        sideBar.titleLabel.textColor = .black
-//        sideBar.backgroundColor = .red
-//        sideBar.arrowLabel.textColor = .white
+        if self.model.screenXcoordinate == .right {
+            if model.titleString.count > 10 {
+                while model.titleString.count != 10 {
+                    model.titleString.removeLast()
+                }
+            }
+            if model.labelType == .downToUp {
+                globSidebarView!.LeftTitleLabel.text = model.titleString + " ^"
+            } else if model.labelType == .upToDown {
+                globSidebarView!.LeftTitleLabel.text = "⌄ " + model.titleString
+            }
+            //modele göre diğer elementlerin assign edilmesi gerek left mini viewa
+        } else if self.model.screenXcoordinate == .left {
+            if model.titleString.count > 10 {
+                while model.titleString.count != 10 {
+                    model.titleString.removeLast()
+                }
+            }
+            if model.labelType == .downToUp {
+                globSidebarView!.rightTitleLabel.text = model.titleString + " ⌄"
+            } else if model.labelType == .upToDown {
+                globSidebarView!.rightTitleLabel.text = "^ " + model.titleString
+            }
+            //modele göre diğer elementlerin assign edilmesi gerek left mini viewa
+        }
 
     }
     
     @objc func imageClicked(_ sender: UITapGestureRecognizer? = nil) {
         print("image a basıldı")
-       
-
-
     }
 
     @objc func viewClicked(_ sender: UITapGestureRecognizer? = nil) {
@@ -128,16 +151,43 @@ class visilabsSideBarViewController : UIViewController {
         UIView.animate(withDuration: 0.5, animations: { [self] in
             if sideBarOpen {
                 self.window?.layer.position = sideBarFirstPosition!
-                globSidebarView?.titleLabel.text = titleStr + " ^"
+                if model.screenXcoordinate == .right {
+                    if model.labelType == .downToUp {
+                        globSidebarView?.LeftTitleLabel.text = model.titleString + " ^"
+                    } else if model.labelType == .upToDown {
+                        globSidebarView?.LeftTitleLabel.text = "⌄ " + model.titleString
+                    }
+                } else if model.screenXcoordinate == .left {
+                    if model.labelType == .downToUp {
+                        globSidebarView?.rightTitleLabel.text = model.titleString + " ⌄"
+                    } else if model.labelType == .upToDown {
+                        globSidebarView?.rightTitleLabel.text = "^ " + model.titleString
+                    }
+                    
+                }
             } else {
-                if let winPos = self.window?.layer.position {
-                    self.window?.layer.position = CGPoint(x: winPos.x - globSidebarView!.width + miniSideBarWidth  , y: winPos.y)
-                    globSidebarView?.titleLabel.text = titleStr + " ⌄"
+                if model.screenXcoordinate == .right {
+                    if let winPos = self.window?.layer.position {
+                        self.window?.layer.position = CGPoint(x: winPos.x - globSidebarView!.width + self.model.miniSideBarWidth  , y: winPos.y)
+                        if model.labelType == .downToUp {
+                            globSidebarView?.LeftTitleLabel.text = model.titleString + " ⌄"
+                        } else if model.labelType == .upToDown {
+                            globSidebarView?.LeftTitleLabel.text = "^ " + model.titleString
+                        }
+                    }
+                } else if model.screenXcoordinate == .left {
+                    if let winPos = self.window?.layer.position {
+                        self.window?.layer.position = CGPoint(x: winPos.x + globSidebarView!.width - self.model.miniSideBarWidth  , y: winPos.y)
+                        if model.labelType == .downToUp {
+                            globSidebarView?.rightTitleLabel.text = model.titleString + " ^"
+                        } else if model.labelType == .upToDown {
+                            globSidebarView?.rightTitleLabel.text = "⌄ " + model.titleString
+                        }
+                    }
                 }
             }
         })
         sideBarOpen = !sideBarOpen
-
     }
     
     
@@ -157,16 +207,25 @@ class visilabsSideBarViewController : UIViewController {
             bounds = UIScreen.main.bounds
         }
         let SideBarframeWidth = bounds.size.width / 2
-        //ortada istiyorsak
-        let frameY = bounds.size.height / 2 - sideBarHeight / 2
-        //aşağıda istersek
-        //let frameY = bounds.size.height - sideBarHeight * 1.5
-        //yukarıda istersek
-        //let frameY = bounds.size.height/2 - sideBarHeight * 1.5
-
         
-    
-        let frame = CGRect(origin: CGPoint(x: bounds.maxX-miniSideBarWidth, y: CGFloat(frameY)), size: CGSize(width: SideBarframeWidth, height: CGFloat(sideBarHeight)))
+        var frameY = bounds.size.height / 2 - self.model.sideBarHeight / 2
+
+        if self.model.screenYcoordinate == .top {
+            frameY = bounds.size.height/2 - self.model.sideBarHeight * 1.5
+        } else if self.model.screenYcoordinate == .bottom {
+            frameY = bounds.size.height - self.model.sideBarHeight * 1.5
+        } else if self.model.screenYcoordinate == .middle {
+            frameY = bounds.size.height / 2 - self.model.sideBarHeight / 2
+        }
+
+        var frame = CGRect(origin: CGPoint(x: bounds.maxX-self.model.miniSideBarWidth, y: CGFloat(frameY)), size: CGSize(width: SideBarframeWidth, height: CGFloat(self.model.sideBarHeight)))
+        
+        if self.model.screenXcoordinate == .right {
+            frame = CGRect(origin: CGPoint(x: bounds.maxX-self.model.miniSideBarWidth, y: CGFloat(frameY)), size: CGSize(width: SideBarframeWidth, height: CGFloat(self.model.sideBarHeight)))
+        } else if self.model.screenXcoordinate == .left {
+            frame = CGRect(origin: CGPoint(x: bounds.minX-SideBarframeWidth+model.miniSideBarWidth, y: CGFloat(frameY)), size: CGSize(width: SideBarframeWidth, height: CGFloat(self.model.sideBarHeight)))
+        }
+        
         if #available(iOS 13.0, *) {
             let windowScene = sharedUIApplication
                 .connectedScenes
@@ -206,4 +265,52 @@ class visilabsSideBarViewController : UIViewController {
             self.position = self.window?.layer.position
         })
     }
+    
+    func addTapGestureToSideBarMiniView() {
+        if self.model.screenXcoordinate == .right {
+            let tap = UITapGestureRecognizer(target: self, action: #selector(self.viewClicked(_:)))
+            globSidebarView?.LeftSideBarMiniView.addGestureRecognizer(tap)
+        } else if self.model.screenXcoordinate == .left {
+            let tap = UITapGestureRecognizer(target: self, action: #selector(self.viewClicked(_:)))
+            globSidebarView?.rightSideBarMiniView.addGestureRecognizer(tap)
+        }
+    }
+    
+    
+    func addTapGestureToImageOfGranSideBar() {
+        globSidebarView?.sideBarGrandContentImageView.isUserInteractionEnabled = true
+        let tap = UITapGestureRecognizer(target: self, action: #selector(self.imageClicked(_:)))
+        globSidebarView?.sideBarGrandContentImageView.addGestureRecognizer(tap)
+        
+    }
+}
+
+
+
+class SideBarModel {
+    
+    var isCircle : Bool = false
+    var sideBarHeight = 175.0
+    var miniSideBarWidth = 40.0
+    var titleString : String = "Deneme"
+    var screenYcoordinate : screenYcoordinate?
+    var screenXcoordinate : screenXcoordinate?
+    var labelType : labelType?
+    
+    enum screenYcoordinate {
+        case top
+        case middle
+        case bottom
+    }
+    
+    enum screenXcoordinate {
+        case right
+        case left
+    }
+    
+    enum labelType {
+        case downToUp
+        case upToDown
+    }
+    
 }
