c96a48c107af24b24a3d8cedd7e7e0fdd87ef55e
cicimen <egemengulkilik@gmail.com>
2022-03-21 10:12:20 +0300

    titlePosition, textPosition, buttonPosition and copybuttonPosition arrange

--- Sources/Assets/spintowin.js
+++ Sources/Assets/spintowin.js
@@ -2810,7 +2810,7 @@ function SpinToWin(config) {
 		this.convertConfigJson()
 	}
 	this.container = document.getElementById("container");
-	if(this.config.taTemplate == "full_spin") {
+	if (this.config.taTemplate == "full_spin") {
 		this.wheelContainerId = "wheel-container-full";
 		document.getElementById("wheel-container-half").remove();
 	} else {
@@ -2852,7 +2852,7 @@ function SpinToWin(config) {
 	this.config.statusBarHeight = window.screen.height - window.innerHeight;
 	this.config.windowHeightWidthRatio = window.innerHeight / window.innerWidth;
 	if (this.config.taTemplate == "full_spin") {
-		this.config.wheelContainerMarginLeft = this.config.windowHeightWidthRatio > 1.6 ? window.innerWidth / 6 : window.innerWidth / 5;
+		this.config.wheelContainerMarginLeft = this.config.windowHeightWidthRatio > 1.6 ? (window.innerWidth / 6) : (window.innerWidth / 5);
 		this.config.r = parseFloat(window.innerWidth / 2) - this.config.wheelContainerMarginLeft;
 		this.config.wheelContainerWidth = this.config.windowWidth - this.config.wheelContainerMarginLeft * 2
 	} else {
@@ -2876,11 +2876,20 @@ function SpinToWin(config) {
 	window.spinToWin = this;
 	this.createItems();
 	this.createEasyWheel();
+
+	if(this.config.taTemplate == "full_spin") {
+		this.config.wheelContainerMarginLeft = this.config.wheelContainerMarginLeft - window.innerWidth * 0.06;
+		this.wheelContainer.style.marginLeft = this.config.wheelContainerMarginLeft + "px";
+	}
+
 	this.handleVisibility()
 	var rotates = 4;
 	this.config.totalSliceCount = this.config.items.length * rotates;
 	this.config.mailFormSubmitCompleted = false;
 	this.checkAvailability();
+
+	
+
 };
 
 SpinToWin.prototype.checkAvailability = function () {
@@ -3061,13 +3070,13 @@ SpinToWin.prototype.convertConfigJson = function () {
 	this.config.promocodeTitleCustomFontFamilyAndroid = extendedProps.promocode_title_custom_font_family_android;
 	this.config.copybuttonCustomFontFamilyAndroid = extendedProps.copybutton_custom_font_family_android;
 	this.config.promocodesSoldoutMessageCustomFontFamilyAndroid = extendedProps.promocodes_soldout_message_custom_font_family_android;
-	
+
 	//position
 	this.config.titlePosition = extendedProps.title_position;
 	this.config.textPosition = extendedProps.text_position;
 	this.config.buttonPosition = extendedProps.button_position;
 	this.config.copybuttonPosition = extendedProps.copybutton_position;
-	
+
 	//promocodeBanner
 	this.config.promocodeBannerText = extendedProps.promocode_banner_text;
 	this.config.promocodeBannerTextColor = extendedProps.promocode_banner_text_color;
@@ -3248,6 +3257,40 @@ SpinToWin.prototype.setContent = function () {
 	this.promocodesSoldoutMessageElement.style.fontSize = (this.config.promocodesSoldoutMessageTextSize + 20) + "px";
 	this.promocodesSoldoutMessageElement.style.backgroundColor = this.config.promocodesSoldoutMessageBackgroundColor;
 
+
+	if(this.config.titlePosition == "bottom"){
+		this.titleElement.remove();
+		this.wheelContainer.parentNode.insertBefore(this.titleElement, this.wheelContainer.nextSibling);
+		this.titleElement.style.marginTop = window.innerWidth * 0.88  + "px";
+		if(this.config.textPosition == "bottom"){
+			this.messageElement.remove();
+			this.titleElement.parentNode.insertBefore(this.messageElement, this.titleElement.nextSibling);
+			if(this.config.buttonPosition == "bottom"){
+				this.submitButton.remove();
+				this.messageElement.parentNode.insertBefore(this.submitButton, this.messageElement.nextSibling);
+			}
+		} else {
+			this.submitButton.remove();
+			this.titleElement.parentNode.insertBefore(this.submitButton, this.titleElement.nextSibling);
+		}
+	} else if(this.config.textPosition == "bottom") {
+		this.messageElement.remove();
+		this.wheelContainer.parentNode.insertBefore(this.messageElement, this.wheelContainer.nextSibling);
+		this.messageElement.style.marginTop = window.innerWidth * 0.88  + "px";
+		if(this.config.buttonPosition == "bottom"){
+			this.submitButton.remove();
+			this.messageElement.parentNode.insertBefore(this.submitButton, this.messageElement.nextSibling);
+		}
+	} else if(this.config.buttonPosition == "bottom"){
+		this.submitButton.remove();
+		this.wheelContainer.parentNode.insertBefore(this.submitButton, this.wheelContainer.nextSibling);
+		this.submitButton.style.marginTop = window.innerWidth * 0.88  + "px";
+	} else if(this.config.copybuttonPosition == "bottom"){
+		this.copyButton.remove();
+		this.wheelContainer.parentNode.insertBefore(this.copyButton, this.wheelContainer.nextSibling);
+		this.copyButton.style.marginTop = window.innerWidth * 0.88  + "px";
+	}
+
 	this.container.addEventListener("click", function (event) {
 		if (event.target.tagName != "INPUT") {
 			document.activeElement.blur()
@@ -3268,7 +3311,7 @@ SpinToWin.prototype.setContent = function () {
 		this.submitButton.addEventListener("click", this.submit);
 	}
 	this.closeButton.addEventListener("click", evt => this.close());
-	this.copyButton.addEventListener("click", evt => this.copyToClipboard())
+	this.copyButton.addEventListener("click", evt => this.copyToClipboard());
 };
 
 SpinToWin.prototype.validateForm = function () {
@@ -3361,13 +3404,20 @@ SpinToWin.prototype.handleVisibility = function () {
 	}
 };
 SpinToWin.prototype.getWheelContainerMarginTop = function () {
+	var marginTop = 10;
 	if (window.innerHeight < 750) {
-		return "10px"
+		marginTop = 10;
 	} else if (window.innerHeight < 1000) {
-		return "30px"
+		marginTop = 30;
 	} else {
-		return "50px"
+		marginTop = 50;
 	}
+
+	if(this.config.taTemplate == "full_spin") {
+		marginTop = marginTop + 15;
+	}
+
+	return marginTop + "px";
 };
 SpinToWin.prototype.styleHandler = function () {
 	this.wheelContainer.style.position = "absolute";
@@ -3386,8 +3436,12 @@ SpinToWin.prototype.styleHandler = function () {
 			this.wheelContainer.style.bottom = -this.config.r + this.config.statusBarHeight + "px"
 		}
 	}
+
 	var styleEl = document.createElement("style"),
-		styleString = "#" + this.wheelContainerId + "{float:left;width:" + config.r + "px;height:" + (2 * this.config.r) + "px}@media only screen and (max-width:2500px){" + "#" + this.wheelContainerId +"{float:unset;width:100%;text-align:center;position:relative}" + "}";
+		styleString = "#" + this.wheelContainerId + "{float:left;width:" + config.r
+			+ "px;height:" + (2 * this.config.r) + "px}@media only screen and (max-width:2500px){"
+			+ "#" + this.wheelContainerId + "{float:unset;width:100%;text-align:center;position:relative}" + "}";
+
 	styleEl.id = "rd-styles";
 	if (!document.getElementById("rd-styles")) {
 		styleEl.innerHTML = styleString;
