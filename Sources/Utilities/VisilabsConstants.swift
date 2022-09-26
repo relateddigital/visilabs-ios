829f0e16772dcf08500064ac8484cc49cc4c2b19
orhun <akmilorhun@gmail.com>
2022-09-26 20:47:23 +0300

    version update

--- Sources/Utilities/VisilabsConstants.swift
+++ Sources/Utilities/VisilabsConstants.swift
@@ -9,7 +9,7 @@ import UIKit
 
 struct VisilabsConstants {
     
-    static let sdkVersion = "3.7.6"
+    static let sdkVersion = "3.7.7"
 
     static let HTTP = "http"
     static let HTTPS = urlConstant.shared.securityTag
