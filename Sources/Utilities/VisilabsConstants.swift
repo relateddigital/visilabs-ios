fed538a0f616ad106400049b72191b1a5b38edbe
Umut Can Alparslan <umut_alparslan@hotmail.com>
2023-04-11 10:32:11 +0300

    spin to win bug fix

--- Sources/Utilities/VisilabsConstants.swift
+++ Sources/Utilities/VisilabsConstants.swift
@@ -9,7 +9,7 @@ import UIKit
 
 struct VisilabsConstants {
     
-    static let sdkVersion = "3.9.0"
+    static let sdkVersion = "3.9.1"
 
     static let HTTP = "http"
     static let HTTPS = urlConstant.shared.securityTag
