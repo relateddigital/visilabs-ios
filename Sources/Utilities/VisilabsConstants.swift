a57d1bd1e35c3f9bf969304df84f5722366697ee
Umut Can Alparslan <umut_alparslan@hotmail.com>
2023-08-18 15:24:22 +0300

    Add UTM Parameters to All Events

--- Sources/Utilities/VisilabsConstants.swift
+++ Sources/Utilities/VisilabsConstants.swift
@@ -9,7 +9,7 @@ import UIKit
 
 struct VisilabsConstants {
     
-    static let sdkVersion = "4.0.1"
+    static let sdkVersion = "4.0.2"
     static let sdkType = "native"
 
     static let HTTP = "http"
