f9190c06da84a4902d71e85d9c37e4d60401a3a4
orhun <akmilorhun@gmail.com>
2023-09-15 16:36:22 +0300

    location permission defualt param

--- Sources/Visilabs.swift
+++ Sources/Visilabs.swift
@@ -23,7 +23,7 @@ public class Visilabs {
                                 channel: String = "IOS",
                                 requestTimeoutInSeconds: Int = 30,
                                 geofenceEnabled: Bool = false,
-                                askLocationPermmissionAtStart: Bool = true,
+                                askLocationPermmissionAtStart: Bool = false,
                                 maxGeofenceCount: Int = 20,
                                 isIDFAEnabled: Bool = true,
                                 loggingEnabled: Bool = false,
