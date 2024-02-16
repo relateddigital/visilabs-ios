48a5da774477dcbde52f4349f8888c1ce151cba2
cicimen <egemengulkilik@gmail.com>
2024-02-16 17:14:00 +0300

    Example project RecommendationViewController implement

--- Example/VisilabsExample/ViewControllers/RecommendationViewController.swift
+++ Example/VisilabsExample/ViewControllers/RecommendationViewController.swift
@@ -17,12 +17,9 @@ class RecommendationViewController: FormViewController {
         super.viewDidLoad()
         initializeForm()
     }
-    var filtersSection = Section("FILTERS")
-    var filterSections: [Section] = []
     
     private func initializeForm() {
-        form +++
-        Section("Recommendation".uppercased(with: Locale(identifier: "en_US")))
+        form +++ Section("Recommendation".uppercased(with: Locale(identifier: "en_US")))
         <<< ButtonRow {
             $0.title = "Test"
         }.onCellSelection { _, _ in
@@ -30,17 +27,22 @@ class RecommendationViewController: FormViewController {
             properties["prop1"] = "prop1val"
             properties["prop1"] = "prop2val"
             var filters = [VisilabsRecommendationFilter]()
-            filters.append(VisilabsRecommendationFilter(attribute:.PRODUCTNAME , filterType: .like, value: "a"))
+            filters.append(
+                VisilabsRecommendationFilter(attribute: .PRODUCTNAME, filterType: .like, value: "a"))
             
-            filters.append(VisilabsRecommendationFilter(attribute:.PRODUCTCODE,
-                                                        filterType: .equals,
-                                                        value: "000000300079892,000000400206545,000000400210042"))
+            filters.append(
+                VisilabsRecommendationFilter(
+                    attribute: .PRODUCTCODE,
+                    filterType: .equals,
+                    value: "000000300079892,000000400206545,000000400210042"))
             
             //filters.append(VisilabsRecommendationFilter(attribute: .PRODUCTNAME, filterType: .like, value: "laptop"))
             
-            Visilabs.callAPI().recommend(zoneID: "1", productCode: "",
-                                         filters: filters,
-                                         properties: properties) { response in
+            Visilabs.callAPI().recommend(
+                zoneID: "1", productCode: "",
+                filters: filters,
+                properties: properties
+            ) { response in
                 if let error = response.error {
                     print(error)
                 } else {
@@ -70,67 +72,96 @@ class RecommendationViewController: FormViewController {
             $0.value = "asd-123"
         }
         
-        +++ filtersSection
+        form +++ Section("Search Recommendation".uppercased(with: Locale(identifier: "en_US")))
+        <<< TextRow("keyword") {
+            $0.title = "Keyword"
+            $0.add(rule: RuleRequired(msg: "\($0.tag!) required"))
+            $0.placeholder = "Keyword"
+            $0.value = ""
+        }
+        <<< TextRow("searchType") {
+            $0.title = "Search Type"
+            $0.placeholder = "Search Type"
+            $0.value = "web"
+        }
         <<< ButtonRow {
-            $0.title = "Add Filter"
+            $0.title = "Search"
         }.onCellSelection { _, _ in
-            self.addFilterSection()
-        }
-        
-        +++ Section()
-        <<< LabelRow {
-            $0.title = "not implemented yet"
-            //            $0.cell.contentView.backgroundColor = .white
-            //            $0.cell.backgroundColor = .white
-            //            $0.cell.textLabel?.textColor = .black
-            //            $0.cell.textLabel?.textAlignment = .left
-            $0.disabled = true
-        }
-        <<< ButtonRow {
-            $0.title = "recommend"
-            $0.disabled = true
-        }
-        
-        <<< ButtonRow {
-            $0.title = "Searchrecommendation"
-            $0.disabled = false
-        }.onCellSelection { _, _ in
-            self.searchRecocemmendation()
+            
+            var keyword: String = ""
+            var searchType: String = ""
+            
+            if let keywordTextRow = self.form.rowBy(tag: "keyword") as TextRow? {
+                keyword = (keywordTextRow.value ?? "").trimmingCharacters(in: .whitespacesAndNewlines)
+            }
+            
+            if let searchTypeTextRow = self.form.rowBy(tag: "searchType") as TextRow? {
+                searchType = (searchTypeTextRow.value ?? "").trimmingCharacters(
+                    in: .whitespacesAndNewlines)
+            }
+            
+            Visilabs.callAPI().searcRecommendation(keyword: keyword, searchType: searchType) {
+                response in
+                print(response)
+            }
+            
         }
     }
-    
-    private func searchRecocemmendation() {
-        Visilabs.callAPI().searcRecommendation(keyword: "", searchType: "web") { response in
-            print(response)
-        }
-    }
-    
-    private func addFilterSection() {
-        let filterSection = Section("FILTER \(filterSections.count + 1)") { section in
-            section.header = {
-                var header = HeaderFooterView<UIView>(.callback({
-                    let view = UILabel(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
-                    view.text = "╳"
-                    view.textAlignment = .right
-                    view.font = .systemFont(ofSize: 20.0, weight: .bold)
-                    view.textColor = .red
-                    let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self.filtersSection,
-                                                                             action: #selector(self.removeSection))
-                    view.addGestureRecognizer(tap)
-                    return view
-                }))
-                header.height = { 50 }
-                return header
-            }()
-        }
-        filterSections.append(filterSection)
-        //print(filtersSection.index)
-        form.insert(filterSection, at: filtersSection.index! + filterSections.count)
-    }
-    
-    @objc func removeSection() {
-        //        filterSections.remove(at: filtersSection.index! + filtersSection.count)
-        filtersSection.remove(at: filtersSection.index! + filtersSection.count)
-    }
-    
 }
+
+
+/*
+ //var filtersSection = Section("FILTERS")
+ //var filterSections: [Section] = []
+ 
+ +++ filtersSection
+ <<< ButtonRow {
+ $0.title = "Add Filter"
+ }.onCellSelection { _, _ in
+ self.addFilterSection()
+ }
+ 
+ +++ Section()
+ <<< LabelRow {
+ $0.title = "not implemented yet"
+ //            $0.cell.contentView.backgroundColor = .white
+ //            $0.cell.backgroundColor = .white
+ //            $0.cell.textLabel?.textColor = .black
+ //            $0.cell.textLabel?.textAlignment = .left
+ $0.disabled = true
+ }
+ <<< ButtonRow {
+ $0.title = "recommend"
+ $0.disabled = true
+ }
+ */
+
+/*
+ private func addFilterSection() {
+ let filterSection = Section("FILTER \(filterSections.count + 1)") { section in
+ section.header = {
+ var header = HeaderFooterView<UIView>(.callback({
+ let view = UILabel(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+ view.text = "╳"
+ view.textAlignment = .right
+ view.font = .systemFont(ofSize: 20.0, weight: .bold)
+ view.textColor = .red
+ let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self.filtersSection,
+ action: #selector(self.removeSection))
+ view.addGestureRecognizer(tap)
+ return view
+ }))
+ header.height = { 50 }
+ return header
+ }()
+ }
+ filterSections.append(filterSection)
+ //print(filtersSection.index)
+ form.insert(filterSection, at: filtersSection.index! + filterSections.count)
+ }
+ 
+ @objc func removeSection() {
+ //        filterSections.remove(at: filtersSection.index! + filtersSection.count)
+ filtersSection.remove(at: filtersSection.index! + filtersSection.count)
+ }
+ */
