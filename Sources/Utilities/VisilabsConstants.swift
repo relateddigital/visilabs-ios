cc2ca82fff8bf7dc0700c51b4ac98f170647eee5
cicimen <egemengulkilik@gmail.com>
2024-02-17 21:46:05 +0300

    VisilabsSearchRecommendationResponse converted to Codable protocol for json encoding

--- Sources/Utilities/VisilabsConstants.swift
+++ Sources/Utilities/VisilabsConstants.swift
@@ -9,7 +9,7 @@ import UIKit
 
 struct VisilabsConstants {
     
-    static let sdkVersion = "4.1.9"
+    static let sdkVersion = "4.2.0"
     static let sdkTypeKey = "sdk_type"
 
     static let HTTP = "http"
