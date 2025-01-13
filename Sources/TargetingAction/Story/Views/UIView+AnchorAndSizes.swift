823b4f9025937e7357db1c00ae63e6b5adad9424
orhun <akmilorhun@gmail.com>
2025-01-13 16:39:13 +0300

    custom banner view

--- Sources/TargetingAction/Story/Views/UIView+AnchorAndSizes.swift
+++ Sources/TargetingAction/Story/Views/UIView+AnchorAndSizes.swift
@@ -58,3 +58,35 @@ extension UIView {
         return frame.height
     }
 }
+
+
+extension UIView {
+    func setDashedBorderView(width: CGFloat = 2, color: UIColor = .black) {
+        
+        let dashedBorderKey = "dashedBorder"
+        
+        if let subLayers = self.layer.sublayers {
+            for element in subLayers {
+                if element.name == dashedBorderKey {
+                    element.removeFromSuperlayer()
+                }
+            }
+        }
+        
+        let shapeLayer: CAShapeLayer = CAShapeLayer()
+        let frameSize = self.frame.size
+        let shapeRect = CGRect(x: 0, y: 0, width: frameSize.width, height: frameSize.height)
+
+        shapeLayer.bounds = shapeRect
+        shapeLayer.position = CGPoint(x: frameSize.width/2, y: frameSize.height/2)
+        shapeLayer.fillColor = UIColor.clear.cgColor
+        shapeLayer.strokeColor = color.cgColor
+        shapeLayer.lineWidth = width
+        shapeLayer.lineJoin = CAShapeLayerLineJoin.round
+        shapeLayer.lineDashPattern = [4, 4]
+        shapeLayer.path = UIBezierPath(roundedRect: shapeRect, cornerRadius: 5).cgPath
+        shapeLayer.name = dashedBorderKey
+
+        self.layer.addSublayer(shapeLayer)
+    }
+}
