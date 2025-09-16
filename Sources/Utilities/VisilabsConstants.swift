bfb3c65db7fc9d58d4ef50fcb4cfb5d1ca6b5b02
orhun <akmilorhun@gmail.com>
2025-09-16 16:28:31 +0300

    Version Update && IOS 26 Fixes

--- Sources/Utilities/VisilabsConstants.swift
+++ Sources/Utilities/VisilabsConstants.swift
@@ -9,7 +9,7 @@ import UIKit
 
 struct VisilabsConstants {
     
-    static let sdkVersion = "4.3.33"
+    static let sdkVersion = "4.3.34"
     static let sdkTypeKey = "sdk_type"
 
     static let HTTP = "http"
