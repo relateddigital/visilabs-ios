7f2dd283658c803b4ba572005e1127f4f91f0049
orhun <akmilorhun@gmail.com>
2025-10-10 11:33:51 +0300

    Version Update

--- Sources/Utilities/VisilabsConstants.swift
+++ Sources/Utilities/VisilabsConstants.swift
@@ -9,7 +9,7 @@ import UIKit
 
 struct VisilabsConstants {
     
-    static let sdkVersion = "4.3.38"
+    static let sdkVersion = "4.3.39"
     static let sdkTypeKey = "sdk_type"
 
     static let HTTP = "http"
