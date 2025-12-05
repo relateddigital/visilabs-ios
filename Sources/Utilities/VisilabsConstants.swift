2bc58be50cd2fc075df75e94d6c03ca8b9553ec3
Umut Can Alparslan <umut_alparslan@hotmail.com>
2025-12-05 11:46:55 +0300

    version update

--- Sources/Utilities/VisilabsConstants.swift
+++ Sources/Utilities/VisilabsConstants.swift
@@ -9,7 +9,7 @@ import UIKit
 
 struct VisilabsConstants {
     
-    static let sdkVersion = "4.3.43"
+    static let sdkVersion = "4.3.44"
     static let sdkTypeKey = "sdk_type"
 
     static let HTTP = "http"
